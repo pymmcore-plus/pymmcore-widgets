f9057afbd0d7994caa047729be22bae06d183274
fdrgsp <federico.gasparoli@gmail.com>
2022-07-11 15:44:37 -0400

    fix: update __init__.py

--- src/pymmcore_widgets/__init__.py
+++ src/pymmcore_widgets/__init__.py
@@ -8,6 +8,7 @@ except PackageNotFoundError:
 
 from ._device_widget import DeviceWidget, StateDeviceWidget
 from ._exposure_widget import DefaultCameraExposureWidget, ExposureWidget
+from ._load_system_cfg_widget import ConfigurationWidget
 from ._presets_widget import PresetsWidget
 from ._property_browser import PropertyBrowser, _PropertyTable
 from ._property_widget import PropertyWidget, make_property_value_widget
@@ -22,4 +23,5 @@ __all__ = [
     "PresetsWidget",
     "ExposureWidget",
     "DefaultCameraExposureWidget",
+    "ConfigurationWidget",
 ]
