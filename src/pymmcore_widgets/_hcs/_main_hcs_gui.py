58b027da91e353bdec92b4d387077a203c104dc0
fdrgsp <federico.gasparoli@gmail.com>
2022-10-09 09:00:20 -0400

    fix: self.tabwidget

--- src/pymmcore_widgets/_hcs/_main_hcs_gui.py
+++ src/pymmcore_widgets/_hcs/_main_hcs_gui.py
@@ -45,10 +45,9 @@ class HCSGui(QWidget):
         scroll = QScrollArea()
         scroll.setWidgetResizable(True)
         scroll.setAlignment(AlignCenter)
-        widgets = self._add_tab_wdg()
-        scroll.setWidget(widgets)
+        self.tabwidget = self._add_tab_wdg()
+        scroll.setWidget(self.tabwidget)
 
-        # layout.addWidget(scroll)
         layout.insertWidget(0, scroll)
 
         bottom_wdg = self._create_bottom_wdg()
@@ -62,12 +61,10 @@ class HCSGui(QWidget):
         select_plate_tab = self._create_plate_and_fov_tab()
         calibration = self._create_calibration_tab()
         self.ch_and_pos_list = MDAWidget(parent=self)
-        # self.saving_tab = self._create_save_wdg()
 
         tab.addTab(select_plate_tab, "  Plate and FOVs Selection  ")
         tab.addTab(calibration, "  Plate Calibration  ")
         tab.addTab(self.ch_and_pos_list, "  Channel and Positions List  ")
-        # tab.addTab(self.saving_tab, "  Saving  ")
 
         return tab
 
@@ -241,81 +238,3 @@ class HCSGui(QWidget):
         combo_wdg.setLayout(wp_combo_layout)
 
         return combo_wdg
-
-    # def _create_save_wdg(self) -> QWidget:
-
-    #     wdg = QWidget()
-    #     wdg_layout = QVBoxLayout()
-    #     wdg_layout.setSpacing(0)
-    #     wdg_layout.setContentsMargins(10, 10, 10, 10)
-    #     wdg.setLayout(wdg_layout)
-    #     save_group = self._create_save_group()
-    #     wdg_layout.addWidget(save_group)
-
-    #     verticalSpacer = QSpacerItem(
-    #           20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding
-    #       )
-    #     wdg_layout.addItem(verticalSpacer)
-
-    #     return wdg
-
-    # def _create_save_group(self) -> QGroupBox:
-
-    #     self.save_hcs_groupBox = QGroupBox(title="Save HCS Acquisition")
-    #     self.save_hcs_groupBox.setSizePolicy(
-    #         QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
-    #     )
-    #     self.save_hcs_groupBox.setCheckable(True)
-    #     self.save_hcs_groupBox.setChecked(False)
-    #     group_layout = QVBoxLayout()
-    #     group_layout.setSpacing(10)
-    #     group_layout.setContentsMargins(10, 10, 10, 10)
-    #     self.save_hcs_groupBox.setLayout(group_layout)
-
-    #     # directory
-    #     dir_group = QWidget()
-    #     dir_group_layout = QHBoxLayout()
-    #     dir_group_layout.setSpacing(5)
-    #     dir_group_layout.setContentsMargins(0, 10, 0, 5)
-    #     dir_group.setLayout(dir_group_layout)
-    #     lbl_sizepolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-    #     min_lbl_size = 80
-    #     btn_sizepolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-    #     dir_lbl = QLabel(text="Directory:")
-    #     dir_lbl.setMinimumWidth(min_lbl_size)
-    #     dir_lbl.setSizePolicy(lbl_sizepolicy)
-    #     self.dir_lineEdit = QLineEdit()
-    #     self.browse_save_Button = QPushButton(text="...")
-    #     self.browse_save_Button.setSizePolicy(btn_sizepolicy)
-    #     self.browse_save_Button.clicked.connect(self._set_dir)
-    #     dir_group_layout.addWidget(dir_lbl)
-    #     dir_group_layout.addWidget(self.dir_lineEdit)
-    #     dir_group_layout.addWidget(self.browse_save_Button)
-
-    #     # filename
-    #     fname_group = QWidget()
-    #     fname_group_layout = QHBoxLayout()
-    #     fname_group_layout.setSpacing(5)
-    #     fname_group_layout.setContentsMargins(0, 5, 0, 10)
-    #     fname_group.setLayout(fname_group_layout)
-    #     fname_lbl = QLabel(text="File Name: ")
-    #     fname_lbl.setMinimumWidth(min_lbl_size)
-    #     fname_lbl.setSizePolicy(lbl_sizepolicy)
-    #     self.fname_lineEdit = QLineEdit()
-    #     self.fname_lineEdit.setText("HCS")
-    #     fname_group_layout.addWidget(fname_lbl)
-    #     fname_group_layout.addWidget(self.fname_lineEdit)
-
-    #     group_layout.addWidget(dir_group)
-    #     group_layout.addWidget(fname_group)
-
-    #     return self.save_hcs_groupBox
-
-    # def _set_dir(self) -> None:
-
-    #     # set the directory
-    #     self.dir = QFileDialog(self)
-    #     self.dir.setFileMode(QFileDialog.DirectoryOnly)
-    #     self.save_dir = QFileDialog.getExistingDirectory(self.dir)
-    #     self.dir_lineEdit.setText(self.save_dir)
-    #     self.parent_path = Path(self.save_dir)
