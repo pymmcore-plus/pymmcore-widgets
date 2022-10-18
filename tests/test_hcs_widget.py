d922a1d218fbd77f2734169b7a933f11d5f89bd2
fdrgsp <federico.gasparoli@gmail.com>
2022-10-18 11:31:40 -0400

    test: update

--- tests/test_hcs_widget.py
+++ tests/test_hcs_widget.py
@@ -14,6 +14,16 @@ if TYPE_CHECKING:
     from pytestqt.qtbot import QtBot
 
 
+def _get_image_size(mmc):
+    _cam_x = mmc.getROI(mmc.getCameraDevice())[-2]
+    _cam_y = mmc.getROI(mmc.getCameraDevice())[-1]
+    assert _cam_x == 512
+    assert _cam_y == 512
+    _image_size_mm_x = (_cam_x * mmc.getPixelSizeUm()) / 1000
+    _image_size_mm_y = (_cam_y * mmc.getPixelSizeUm()) / 1000
+    return _image_size_mm_x, _image_size_mm_y
+
+
 def test_hcs_plate_selection(qtbot: QtBot):
     hcs = HCSWidget()
     qtbot.add_widget(hcs)
@@ -80,42 +90,43 @@ def test_hcs_plate_selection(qtbot: QtBot):
     hcs.plate.close()
 
 
-def test_hcs_fov_selection(qtbot: QtBot, global_mmcore: CMMCorePlus):
-
+def test_hcs_fov_selection_FOVPoints_size(qtbot: QtBot, global_mmcore: CMMCorePlus):
     mmc = global_mmcore
     hcs = HCSWidget()
     qtbot.add_widget(hcs)
 
-    def _get_image_size():
-        _cam_x = mmc.getROI(mmc.getCameraDevice())[-2]
-        _cam_y = mmc.getROI(mmc.getCameraDevice())[-1]
-        assert _cam_x == 512
-        assert _cam_y == 512
-        _image_size_mm_x = (_cam_x * mmc.getPixelSizeUm()) / 1000
-        _image_size_mm_y = (_cam_y * mmc.getPixelSizeUm()) / 1000
-        return _image_size_mm_x, _image_size_mm_y
-
     assert hcs.tabwidget.currentIndex() == 0
     assert hcs.tabwidget.tabText(0) == "  Plate and FOVs Selection  "
-
-    hcs.wp_combo.setCurrentText("standard 6")
-    assert hcs.wp_combo.currentText() == "standard 6"
-    assert len(hcs.scene.items()) == 6
-    assert not [item for item in hcs.scene.items() if item.isSelected()]
-
-    mmc.setProperty("Objective", "Label", "Nikon 10X S Fluor")
-    assert mmc.getPixelSizeUm() == 1.0
-    _image_size_mm_x, _image_size_mm_y = _get_image_size()
-    assert _image_size_mm_x == 0.512
-    assert _image_size_mm_y == 0.512
-
-    # center
     assert hcs.FOV_selector.tab_wdg.currentIndex() == 0
     assert hcs.FOV_selector.tab_wdg.tabText(0) == "Center"
+
+    hcs.wp_combo.setCurrentText("standard 6")
+
     scene_width = hcs.FOV_selector.scene.sceneRect().width()
     scene_height = hcs.FOV_selector.scene.sceneRect().height()
     assert scene_width == 200
     assert scene_height == 200
+
+    assert mmc.getPixelSizeUm() == 1.0
+    _image_size_mm_x, _image_size_mm_y = _get_image_size(mmc)
+    assert _image_size_mm_x == 0.512
+    assert _image_size_mm_y == 0.512
+    fov, well = list(hcs.FOV_selector.scene.items())
+    assert isinstance(fov, FOVPoints)
+    assert isinstance(well, QGraphicsEllipseItem)
+    assert fov._getPositionsInfo() == (scene_width / 2, scene_height / 2, 160, 160)
+    well_size_mm_x, well_size_mm_y = hcs.wp.get_well_size()
+    assert fov._x_size == (160 * _image_size_mm_x) / well_size_mm_x
+    assert fov._y_size == (160 * _image_size_mm_y) / well_size_mm_y
+    assert fov._x_size == fov._y_size == 2.3540229885057475
+
+    mmc.setProperty("Objective", "Label", "Nikon 20X Plan Fluor ELWD")
+    with qtbot.waitSignal(mmc.events.pixelSizeChanged):
+        mmc.events.pixelSizeChanged.emit(mmc.getPixelSizeUm())
+    assert mmc.getPixelSizeUm() == 0.5
+    _image_size_mm_x, _image_size_mm_y = _get_image_size(mmc)
+    assert _image_size_mm_x == 0.256
+    assert _image_size_mm_y == 0.256
     items = list(hcs.FOV_selector.scene.items())
     assert len(items) == 2
     fov, well = items
@@ -125,50 +136,54 @@ def test_hcs_fov_selection(qtbot: QtBot, global_mmcore: CMMCorePlus):
     well_size_mm_x, well_size_mm_y = hcs.wp.get_well_size()
     assert fov._x_size == (160 * _image_size_mm_x) / well_size_mm_x
     assert fov._y_size == (160 * _image_size_mm_y) / well_size_mm_y
+    assert fov._x_size == fov._y_size == 1.1770114942528738
 
-    mmc.setProperty("Objective", "Label", "Nikon 20X Plan Fluor ELWD")
-    with qtbot.waitSignal(mmc.events.pixelSizeChanged):
-        mmc.events.pixelSizeChanged.emit(mmc.getPixelSizeUm())
-    assert mmc.getPixelSizeUm() == 0.5
-    _image_size_mm_x, _image_size_mm_y = _get_image_size()
-    assert _image_size_mm_x == 0.256
-    assert _image_size_mm_y == 0.256
 
+def test_hcs_fov_selection_center(qtbot: QtBot, global_mmcore: CMMCorePlus):
+    hcs = HCSWidget()
+    qtbot.add_widget(hcs)
+
+    assert hcs.tabwidget.currentIndex() == 0
+    assert hcs.tabwidget.tabText(0) == "  Plate and FOVs Selection  "
+    assert hcs.FOV_selector.tab_wdg.currentIndex() == 0
+    assert hcs.FOV_selector.tab_wdg.tabText(0) == "Center"
+
+    hcs.wp_combo.setCurrentText("standard 6")
+    assert len(hcs.scene.items()) == 6
     items = list(hcs.FOV_selector.scene.items())
     assert len(items) == 2
     fov, well = items
     assert isinstance(fov, FOVPoints)
     assert isinstance(well, QGraphicsEllipseItem)
-    well_size_mm_x, well_size_mm_y = hcs.wp.get_well_size()
-    assert fov._x_size == (160 * _image_size_mm_x) / well_size_mm_x
-    assert fov._y_size == (160 * _image_size_mm_y) / well_size_mm_y
 
-    print("________")
     hcs.wp_combo.setCurrentText("standard 384")
-    assert hcs.wp_combo.currentText() == "standard 384"
     assert len(hcs.scene.items()) == 384
     items = list(hcs.FOV_selector.scene.items())
     assert len(items) == 2
     fov, well = items
-    print("             ***", fov)
-    print("             ***", well)
     assert isinstance(fov, FOVPoints)
     assert isinstance(well, QGraphicsRectItem)
-    _image_size_mm_x, _image_size_mm_y = _get_image_size()
-    well_size_mm_x, well_size_mm_y = hcs.wp.get_well_size()
-    assert fov._x_size == (160 * _image_size_mm_x) / well_size_mm_x
-    assert fov._y_size == (160 * _image_size_mm_y) / well_size_mm_y
 
-    # random
+
+def test_hcs_fov_selection_random(qtbot: QtBot, global_mmcore: CMMCorePlus):
+    hcs = HCSWidget()
+    qtbot.add_widget(hcs)
+
+    assert hcs.tabwidget.currentIndex() == 0
+    assert hcs.tabwidget.tabText(0) == "  Plate and FOVs Selection  "
     hcs.tabwidget.setCurrentIndex(1)
     assert hcs.FOV_selector.tab_wdg.tabText(1) == "Random"
+
+    hcs.wp_combo.setCurrentText("standard 6")
+    assert len(hcs.scene.items()) == 6
+
     hcs.FOV_selector.number_of_FOV.setValue(3)
     assert len(hcs.FOV_selector.scene.items()) == 5
     items = list(hcs.FOV_selector.scene.items())
     well = items[-1]
     well_area = items[-2]
     fovs = items[:3]
-    assert isinstance(well, QGraphicsRectItem)
+    assert isinstance(well, QGraphicsEllipseItem)
     assert isinstance(well_area, WellArea)
     for i in fovs:
         assert isinstance(i, FOVPoints)
@@ -203,18 +218,30 @@ def test_hcs_fov_selection(qtbot: QtBot, global_mmcore: CMMCorePlus):
     assert fov_1._x != fov_2._x
     assert fov_1._y != fov_2._y
 
-    # grid
+
+def test_hcs_fov_selection_grid(qtbot: QtBot, global_mmcore: CMMCorePlus):
+    mmc = global_mmcore
+    hcs = HCSWidget()
+    qtbot.add_widget(hcs)
+
+    assert hcs.tabwidget.currentIndex() == 0
+    assert hcs.tabwidget.tabText(0) == "  Plate and FOVs Selection  "
     hcs.tabwidget.setCurrentIndex(2)
     assert hcs.FOV_selector.tab_wdg.tabText(2) == "Grid"
+
+    hcs.wp_combo.setCurrentText("standard 384")
+    assert len(hcs.scene.items()) == 384
+
     hcs.FOV_selector.rows.setValue(3)
     hcs.FOV_selector.cols.setValue(3)
     hcs.FOV_selector.spacing_x.setValue(500.0)
     hcs.FOV_selector.spacing_y.setValue(500.0)
     items = list(hcs.FOV_selector.scene.items())
-    assert len(items) == 11
-    well_area = items[-2]
-    assert isinstance(well_area, WellArea)
+    assert len(items) == 10
+    well = items[-1]
+    assert isinstance(well, QGraphicsRectItem)
     well_size_mm_x, well_size_mm_y = hcs.wp.get_well_size()
+    _image_size_mm_x, _image_size_mm_y = _get_image_size(mmc)
     fovs = items[:9]
     for fov in fovs:
         assert isinstance(fov, FOVPoints)
@@ -223,13 +250,14 @@ def test_hcs_fov_selection(qtbot: QtBot, global_mmcore: CMMCorePlus):
 
     fov_1 = cast(FOVPoints, items[4])
     fov_2 = cast(FOVPoints, items[5])
-    assert fov_1._getPositionsInfo() == (100.0, 100.0, 160, 160)
-    assert fov_2._getPositionsInfo() == (130.24, 100.0, 160, 160)
+    cx, cy, w, h = fov_1._getPositionsInfo()
+    assert (round(cx, 2), round(cy, 2), w, h) == (100.00, 100.00, 160, 160)
+    cx, cy, w, h = fov_2._getPositionsInfo()
+    assert (round(cx, 2), round(cy, 2), w, h) == (140.48, 100.00, 160, 160)
 
 
-def test_calibration(qtbot: QtBot, global_mmcore: CMMCorePlus):
+def test_calibration_label(qtbot: QtBot, global_mmcore: CMMCorePlus):
 
-    mmc = global_mmcore
     hcs = HCSWidget()
     qtbot.add_widget(hcs)
 
@@ -282,6 +310,20 @@ def test_calibration(qtbot: QtBot, global_mmcore: CMMCorePlus):
     )
     assert cal.info_lbl.text() == text
 
+
+def test_calibration_1_well(qtbot: QtBot, global_mmcore: CMMCorePlus):
+
+    mmc = global_mmcore
+    hcs = HCSWidget()
+    qtbot.add_widget(hcs)
+
+    cal = hcs.calibration
+
+    hcs.tabwidget.setCurrentIndex(1)
+    assert hcs.tabwidget.tabText(1) == "  Plate Calibration  "
+
+    assert cal.cal_lbl.text() == "Plate non Calibrated!"
+
     hcs.wp_combo.setCurrentText("standard 6")
     assert hcs.wp_combo.currentText() == "standard 6"
     assert len(hcs.scene.items()) == 6
@@ -321,8 +363,13 @@ def test_calibration(qtbot: QtBot, global_mmcore: CMMCorePlus):
         cal._calibrate_plate()
 
     cal.table_1.tb.removeRow(3)
+
+    assert cal._get_well_center(cal.table_1) == (0.0, 0.0)
+
     cal._calibrate_plate()
     assert cal.cal_lbl.text() == "Plate Calibrated!"
+
+    assert not cal.plate_rotation_matrix
 
     # TODO:
     # get coords calibration
