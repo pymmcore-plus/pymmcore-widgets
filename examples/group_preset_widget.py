75c07e67ffc5528c0f7dc0426d84e4eeba33f2e1
fdrgsp <federico.gasparoli@gmail.com>
2022-10-19 10:26:24 -0400

    fix: fix example

--- examples/group_preset_widget.py
+++ examples/group_preset_widget.py
@@ -1,16 +1,12 @@
-from pathlib import Path
-
 from pymmcore_plus import CMMCorePlus
 from qtpy.QtWidgets import QApplication
 
 from pymmcore_widgets import GroupPresetTableWidget
 
-CFG = Path(__file__).parent.parent / "tests" / "test_config.cfg"
-
 app = QApplication([])
 
 mmc = CMMCorePlus().instance()
-mmc.loadSystemConfiguration(CFG)
+mmc.loadSystemConfiguration()
 
 group_preset_wdg = GroupPresetTableWidget()
 group_preset_wdg.show()
