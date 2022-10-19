bc6813638b0cfe2eccfbfb79c475b65212ced1d3
fdrgsp <federico.gasparoli@gmail.com>
2022-10-19 10:26:58 -0400

    fix: fix example

--- examples/pixel_size_widget.py
+++ examples/pixel_size_widget.py
@@ -1,16 +1,12 @@
-from pathlib import Path
-
 from pymmcore_plus import CMMCorePlus
 from qtpy.QtWidgets import QApplication
 
 from pymmcore_widgets import PixelSizeWidget
 
-CFG = Path(__file__).parent.parent / "tests" / "test_config.cfg"
-
 app = QApplication([])
 
 mmc = CMMCorePlus().instance()
-mmc.loadSystemConfiguration(CFG)
+mmc.loadSystemConfiguration()
 
 px_wdg = PixelSizeWidget()
 px_wdg.show()
