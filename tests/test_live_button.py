527902e280191af0840514a0a05534db372b54aa
federico gasparoli <70725613+fdrgsp@users.noreply.github.com>
2022-10-23 10:09:34 -0400

    fix: move LIVE button __init__ args to properties + update test (#31)
    * fix: move button __init__ args to properties + update test
    
    * fix: remove icon_size setter + update test
    
    * style: [pre-commit.ci] auto fixes [...]
    
    * fix: docstrings
    
    * fix: remove camera
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_live_button.py
+++ tests/test_live_button.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from qtpy.QtCore import QSize
+
 from pymmcore_widgets._live_button_widget import LiveButton
 
 if TYPE_CHECKING:
@@ -11,17 +13,13 @@ if TYPE_CHECKING:
 
 def test_live_button_widget(qtbot: QtBot, global_mmcore: CMMCorePlus):
 
-    live_btn = LiveButton(
-        button_text_on_off=("Live", "Stop"),
-        icon_size=40,
-        icon_color_on_off=("green", "magenta"),
-    )
+    live_btn = LiveButton()
 
     qtbot.addWidget(live_btn)
 
     assert live_btn.text() == "Live"
-    assert live_btn.icon_size == 40
-    assert live_btn.icon_color_on == "green"
+    assert live_btn.iconSize() == QSize(30, 30)
+    assert live_btn.icon_color_on == (0, 255, 0)
     assert live_btn.icon_color_off == "magenta"
 
     # test from direct mmcore signals
@@ -44,3 +42,15 @@ def test_live_button_widget(qtbot: QtBot, global_mmcore: CMMCorePlus):
         live_btn.click()
     assert not global_mmcore.isSequenceRunning()
     assert live_btn.text() == "Live"
+
+    live_btn.icon_color_on = "Red"
+    assert live_btn._icon_color_on == "Red"
+    live_btn.icon_color_off = "Green"
+    assert live_btn._icon_color_off == "Green"
+    live_btn.button_text_on = "LIVE"
+    assert live_btn.text() == "LIVE"
+    live_btn.button_text_off = "STOP"
+    global_mmcore.startContinuousSequenceAcquisition(0)
+    assert live_btn.text() == "STOP"
+    global_mmcore.stopSequenceAcquisition()
+    assert live_btn.text() == "LIVE"
