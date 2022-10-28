c7de06756159ba8efeb6567354e864c7d5a1c9ce
federico gasparoli <70725613+fdrgsp@users.noreply.github.com>
2022-10-28 11:43:54 -0400

    docs: Add Examples (#63)
    * feat: update image_widget
    
    * feat: add examples
    
    * Update state_device_widget.py
    
    * fix: update
    
    * feat: add DefaultCameraExposureWidget examplex

---
+++ examples/live_button.py
@@ -0,0 +1,19 @@
+"""
+Check also the 'image_widget.py' example to see the LiveButton
+used in combination of other widgets.
+"""
+
+from pymmcore_plus import CMMCorePlus
+from qtpy.QtWidgets import QApplication
+
+from pymmcore_widgets import LiveButton
+
+app = QApplication([])
+
+mmc = CMMCorePlus().instance()
+mmc.loadSystemConfiguration()
+
+live_btn = LiveButton()
+live_btn.show()
+
+app.exec_()
