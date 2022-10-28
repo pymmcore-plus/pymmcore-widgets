4d4853cbaebde53a99d069291a78c94f3535f23c
Talley Lambert <talley.lambert@gmail.com>
2022-10-28 11:39:58 -0400

    update example

--- examples/mda_widget.py
+++ examples/mda_widget.py
@@ -1,11 +1,9 @@
-import numpy as np
 from pymmcore_plus import CMMCorePlus
 from qtpy.QtWidgets import (
     QApplication,
     QGroupBox,
     QHBoxLayout,
     QLabel,
-    QSizePolicy,
     QVBoxLayout,
     QWidget,
 )
@@ -15,56 +13,64 @@ from pymmcore_widgets import MultiDWidget
 
 
 class MDA(QWidget):
-    """
-    An example widget for the MultiDWidget acquisition widget.
+    """An example of using the MultiDWidget create an useq.MDASequence.
 
-    The MultiDWidget can be used to build a mda experiment.
-    The 'Run' button is linked to the pymmcore-plus 'run_mda' method.
+    The `MultiDWidget` itself does not "run" an experiment, it simply provides a GUI
+    to construct a `useq.MDASequence` object.  This object describes a full
+    multi-dimensional acquisition; it can be passed to the `CMMCorePlus.run_mda` to
+    actually execute the acquisition. See https://github.com/pymmcore-plus/useq-schema
+    and https://github.com/pymmcore-plus/pymmcore-plus for details of the corresponding
+    schema and methods.
 
-    In this example, the whole acquisition sequence as well as each acquisition event
-    are dysplayed in two QLabel widgets.
+    In this example, we've connected callbacks to the CMMCorePlus object's `mda` events
+    to print out the current state of the acquisition.
     """
 
     def __init__(self) -> None:
         super().__init__()
 
-        self.setMaximumWidth(800)
-
+        # get the CMMCore instance and load the default config
         mmc = CMMCorePlus.instance()
         mmc.loadSystemConfiguration()
 
+        # connect MDA acquisition events to local callbacks
+        # in this example we're just printing the current state of the acquisition
         mmc.mda.events.sequenceStarted.connect(self._on_start)
-        mmc.mda.events.frameReady.connect(self._on_event)
+        mmc.mda.events.frameReady.connect(self._on_frame)
         mmc.mda.events.sequenceFinished.connect(self._on_end)
         mmc.mda.events.sequencePauseToggled.connect(self._on_pause)
 
-        mda_wdg = QGroupBox()
-        mda_wdg.setMaximumWidth(500)
-        mda_wdg.setLayout(QVBoxLayout())
+        # instantiate the MultiDWidget, and a couple lables for feedback
         self.mda = MultiDWidget()
+        self.current_sequence = QLabel('... enter info and click "Run"')
+        self.current_event = QLabel("... current event info will appear here")
+
+        # below here is just GUI layout stuff
+        mda_wdg = QGroupBox()
+        mda_wdg.setMaximumWidth(600)
+        mda_wdg.setLayout(QVBoxLayout())
+        mda_wdg.layout().setContentsMargins(0, 0, 0, 0)
         mda_wdg.layout().addWidget(self.mda)
 
         lbl_wdg = QGroupBox()
-        lbl_wdg.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
+        lbl_wdg.setMinimumWidth(275)
         lbl_wdg.setLayout(QVBoxLayout())
-        self.lbl_sequence = QLabel(text="\nACQUISITION SEQUENCE:")
-        self.lbl_event = QLabel(text="ACQUISITION EVENT:")
-        lbl_wdg.layout().addWidget(self.lbl_sequence)
-        lbl_wdg.layout().addWidget(self.lbl_event)
+        lbl_wdg.layout().addWidget(QLabel(text="<h3>ACQUISITION SEQUENCE</h3>"))
+        lbl_wdg.layout().addWidget(self.current_sequence)
+        lbl_wdg.layout().addWidget(QLabel(text="<h3>ACQUISITION EVENT</h3>"))
+        lbl_wdg.layout().addWidget(self.current_event)
 
         self.setLayout(QHBoxLayout())
         self.layout().addWidget(mda_wdg)
         self.layout().addWidget(lbl_wdg)
 
     def _on_start(self, sequence: MDASequence) -> None:
-        self.lbl_sequence.clear()
-        self.lbl_event.clear()
-        self.lbl_sequence.setText(f"\nACQUISITION SEQUENCE:\n\n{sequence.yaml()}")
+        """Called when the MDA sequence starts."""
+        self.current_sequence.setText(sequence.yaml())
 
-    def _on_event(self, image: np.ndarray, event: MDAEvent) -> None:
-        self.lbl_event.clear()
-        self.lbl_event.setText(
-            "ACQUISITION EVENT:\n\n"
+    def _on_frame(self, image, event: MDAEvent) -> None:
+        """Called each time a frame is acquired."""
+        self.current_event.setText(
             f"index: {event.index}\n"
             f"channel: {event.channel.config}\n"
             f"exposure: {event.exposure}\n"
@@ -73,15 +79,13 @@ class MDA(QWidget):
         )
 
     def _on_end(self) -> None:
-        self.lbl_event.clear()
-        self.lbl_event.setText("ACQUISITION FINISHED.")
+        """Called when the MDA sequence ends."""
+        self.current_event.setText("Finished!")
 
     def _on_pause(self, state: bool) -> None:
-        self.lbl_event.clear()
-        if state:
-            self.lbl_event.setText("ACQUISITION EVENT:\n\n" "acquisition paused...")
-        else:
-            self.lbl_event.setText("ACQUISITION EVENT:\n\n" "restarting acquisition...")
+        """Called when the MDA is paused."""
+        txt = "Paused..." if state else "Resumed!"
+        self.current_event.setText(txt)
 
 
 if __name__ == "__main__":
