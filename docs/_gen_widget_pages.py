0b043249c40fefd05290e27792fdc6b6d6b30a97
Talley Lambert <talley.lambert@gmail.com>
2022-11-22 15:07:28 -0500

    docs: wip on images

--- docs/_gen_widget_pages.py
+++ docs/_gen_widget_pages.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 WIDGETS = Path(__file__).parent / "widgets"
+EXAMPLES = Path(__file__).parent.parent / "examples"
 
 
 def _widget_list() -> list[str]:
@@ -25,6 +26,40 @@ def _camel_to_snake(name: str) -> str:
     return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
 
 
+def _example_screenshot(name):
+    path = EXAMPLES / f"{name}.py"
+    if not path.exists():
+        return ""
+
+    from qtpy.QtWidgets import QApplication
+
+    src = path.read_text().strip()
+    src = src.replace("QApplication([])", "QApplication.instance() or QApplication([])")
+    src = src.replace("self.mmc.loadSystemConfiguration()", "")
+    src = src.replace("mmc.loadSystemConfiguration()", "")
+    src = src.replace("app.exec_()", "")
+    try:
+        exec(src, globals(), globals())
+    except Exception:
+        breakpoint()
+
+    app = QApplication.instance() or QApplication([])
+    app.topLevelWidgets()
+    for widget in app.topLevelWidgets():
+        widget.show()
+        widget.grab().save(f"screenshot_{name}.png")
+        widget.close()
+        widget.deleteLater()
+    app.processEvents()
+    app.processEvents()
+    # breakpoint()
+    # w = _w(app)
+    # w.activateWindow()
+    # if width:
+    # w.setFixedWidth(width)
+    # w.grab().save(dest)
+
+
 def generate_widget_pages() -> None:
     """Auto-Generate pages in the widgets folder."""
     from textwrap import dedent
@@ -38,6 +73,8 @@ def generate_widget_pages() -> None:
 
         filename = f"widgets/{widget}.md"
         snake = _camel_to_snake(widget)
+
+        _ = _example_screenshot(snake)
 
         with mkdocs_gen_files.open(filename, "w") as f:
             md = f"""
