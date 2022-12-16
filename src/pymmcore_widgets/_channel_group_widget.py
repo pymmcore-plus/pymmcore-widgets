1e96cd2517fd50bed7b57e4b95049d11b40307e6
fdrgsp <federico.gasparoli@gmail.com>
2022-12-15 21:58:38 -0500

    feat: add ChannelGroupWidget + update ChannelTable

---
+++ src/pymmcore_widgets/_channel_group_widget.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+from pymmcore_plus import CMMCorePlus
+from qtpy.QtWidgets import QComboBox, QWidget
+from superqt.utils import signals_blocked
+
+
+class ChannelGroupWidget(QComboBox):
+    """A QComboBox to follow and control Micro-Manager ChannelGroup."""
+
+    def __init__(
+        self, parent: QWidget | None = None, *, mmcore: CMMCorePlus | None = None
+    ) -> None:
+        super().__init__(parent)
+
+        self._mmc = mmcore or CMMCorePlus.instance()
+
+        # connect mmcore events
+        self._mmc.events.systemConfigurationLoaded.connect(
+            self._update_channel_group_combo
+        )
+        self._mmc.events.channelGroupChanged.connect(self._on_channel_group_changed)
+        self._mmc.events.propertyChanged.connect(self._on_property_changed)
+        self._mmc.events.configGroupDeleted.connect(self._update_channel_group_combo)
+
+        self.currentTextChanged.connect(self._on_channel_group_combo_changed)
+
+        self.destroyed.connect(self._disconnect)
+
+        self._update_channel_group_combo()
+
+    def _update_channel_group_combo(self) -> None:
+        with signals_blocked(self):
+            self.clear()
+            self.addItems(self._mmc.getAvailableConfigGroups())
+            if ch_group := self._mmc.getChannelGroup():
+                self.setCurrentText(ch_group)
+                self.setStyleSheet("")
+            else:
+                self.setStyleSheet("color: magenta;")
+
+    def _on_channel_group_combo_changed(self, group: str) -> None:
+        # use setProperty because setChannelGroup deos not emit a signal
+        self._mmc.setProperty("Core", "ChannelGroup", group)
+
+    def _on_property_changed(self, device: str, property: str, value: str) -> None:
+        if device != "Core" or property != "ChannelGroup":
+            return
+        with signals_blocked(self):
+            if value:
+                self.setCurrentText(value)
+                self.setStyleSheet("")
+            else:
+                self.setStyleSheet("color: magenta;")
+
+    def _on_channel_group_changed(self, group: str) -> None:
+        if group in [self.currentText(), self._mmc.getChannelGroup()]:
+            return
+        self._update_channel_group_combo()
+
+    def _disconnect(self) -> None:
+        self._mmc.events.systemConfigurationLoaded.disconnect(
+            self._update_channel_group_combo
+        )
+        self._mmc.events.channelGroupChanged.disconnect(self._on_channel_group_changed)
+        self._mmc.events.propertyChanged.disconnect(self._on_property_changed)
+        self._mmc.events.configGroupDeleted.disconnect(self._update_channel_group_combo)
