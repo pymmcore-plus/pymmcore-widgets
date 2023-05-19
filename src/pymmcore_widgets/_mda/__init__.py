c270c5c50ea1489d77f53e06ff7c20459e3fa356
fdrgsp <federico.gasparoli@gmail.com>
2023-05-19 15:41:43 -0400

    fix: remove sample explorer wdg

--- src/pymmcore_widgets/_mda/__init__.py
+++ src/pymmcore_widgets/_mda/__init__.py
@@ -2,7 +2,6 @@ from ._channel_table_widget import ChannelTable
 from ._grid_widget import GridWidget
 from ._mda_widget import MDAWidget
 from ._positions_table_widget import PositionTable
-from ._sample_explorer_widget import SampleExplorerWidget
 from ._time_plan_widget import TimePlanWidget
 from ._zstack_widget import ZStackWidget
 
@@ -10,7 +9,6 @@ __all__ = [
     "ChannelTable",
     "GridWidget",
     "MDAWidget",
-    "SampleExplorerWidget",
     "TimePlanWidget",
     "ZStackWidget",
     "PositionTable",
