7144ee4fe5d31d0f8975156bbb94ba5c17a44298
fdrgsp <federico.gasparoli@gmail.com>
2023-06-20 17:16:50 -0400

    test: update tests

--- tests/test_position_table_widget.py
+++ tests/test_position_table_widget.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import contextlib
 from typing import TYPE_CHECKING, Any
 
 import pytest
@@ -12,13 +13,20 @@ if TYPE_CHECKING:
     from pytestqt.qtbot import QtBot
 
 
-def _get_values(table: QTableWidget, row: int):
-    return (
-        table.item(row, 0).text(),
-        table.cellWidget(row, 1).value(),
-        table.cellWidget(row, 2).value(),
-        table.cellWidget(row, 3).value(),
-    )
+P = 0
+X = 1
+Y = 2
+Z = 3
+AF = 4
+GRID = 5
+
+
+def _get_values(table: QTableWidget, row: int) -> list[str | float]:
+    result = [table.item(row, P).text()]
+    for i in [X, Y, Z, AF]:
+        with contextlib.suppress(AttributeError):
+            result.append(table.cellWidget(row, i).value())
+    return result
 
 
 def test_single_position(global_mmcore: CMMCorePlus, qtbot: QtBot):
@@ -36,8 +44,8 @@ def test_single_position(global_mmcore: CMMCorePlus, qtbot: QtBot):
 
     assert tb.rowCount() == 2
 
-    assert _get_values(tb, 0) == ("Pos000", 0.0, 0.0, 0.0)
-    assert _get_values(tb, 1) == ("Pos001", 100.0, 200.0, 0.0)
+    assert _get_values(tb, 0) == ["Pos000", 0.0, 0.0, 0.0]
+    assert _get_values(tb, 1) == ["Pos001", 100.0, 200.0, 0.0]
 
     assert tb.item(1, 0).text() == "Pos001"
     tb.item(0, 0).setText("test")
@@ -64,14 +72,14 @@ def test_replace_pos(global_mmcore: CMMCorePlus, qtbot: QtBot):
     tb = p._table
 
     p.add_button.click()
-    assert _get_values(tb, 0) == ("Pos000", 0.0, 0.0, 0.0)
+    assert _get_values(tb, 0) == ["Pos000", 0.0, 0.0, 0.0]
 
     mmc.setXYPosition(100, 200)
     mmc.setPosition(50)
 
     tb.selectRow(0)
     p.replace_button.click()
-    assert _get_values(tb, 0) == ("Pos000", 100.0, 200.0, 50.0)
+    assert _get_values(tb, 0) == ["Pos000", 100.0, 200.0, 50.0]
 
 
 def test_go_to_pos(global_mmcore: CMMCorePlus, qtbot: QtBot):
@@ -84,7 +92,7 @@ def test_go_to_pos(global_mmcore: CMMCorePlus, qtbot: QtBot):
     mmc.setXYPosition(100, 200)
     mmc.setPosition(50)
     p.add_button.click()
-    assert _get_values(tb, 0) == ("Pos000", 100.0, 200.0, 50.0)
+    assert _get_values(tb, 0) == ["Pos000", 100.0, 200.0, 50.0]
     assert round(mmc.getXPosition()) == 100.0
     assert round(mmc.getYPosition()) == 200.0
     assert round(mmc.getPosition()) == 50.0
@@ -112,6 +120,7 @@ def pos():
         "x": 100.0,
         "y": 200.0,
         "z": 0.0,
+        "autofocus": (None),
         "sequence": {
             "grid_plan": {
                 "columns": 2,
@@ -128,6 +137,7 @@ def pos():
         "x": 10.0,
         "y": 20.0,
         "z": 0.0,
+        "autofocus": None,
         "sequence": {
             "grid_plan": {
                 "columns": 2,
@@ -144,6 +154,7 @@ def pos():
         "x": 0.0,
         "y": 0.0,
         "z": 0.0,
+        "autofocus": None,
         "sequence": {
             "grid_plan": {
                 "bottom": 0.0,
@@ -174,11 +185,11 @@ def test_relative_grid_position(
     mmc.setXYPosition(100, 200)
     p.add_button.click()
     assert tb.rowCount() == 1
-    assert tb.isColumnHidden(4)
+    assert tb.isColumnHidden(GRID)
 
     p._advanced_cbox.setChecked(True)
     assert p._warn_icon.isHidden()
-    assert not tb.isColumnHidden(4)
+    assert not tb.isColumnHidden(GRID)
 
     add_grid_btn, remove_grid_btn = p._get_grid_buttons(0)
     assert remove_grid_btn.isHidden()
@@ -274,3 +285,109 @@ def test_pos_table_set_and_get_state(
     p._advanced_cbox.setChecked(False)
     assert not p._warn_icon.isHidden()
     assert p.value() == [pos_1, pos_2, pos_3]
+
+
+def test_autofocus_position(global_mmcore: CMMCorePlus, qtbot: QtBot):
+    p = PositionTable()
+    qtbot.addWidget(p)
+
+    mmc = global_mmcore
+    tb = p._table
+
+    assert p._autofocus_wdg.value() == {"device_name": None, "use_autofocus": False}
+    assert tb.isColumnHidden(AF)
+    p._autofocus_wdg.setValue({"device_name": "Z1", "use_autofocus": True})
+    assert not tb.isColumnHidden(AF)
+
+    mmc.setXYPosition(100, 200)
+    mmc.setZPosition(50)
+    mmc.setPosition("Z1", 45)
+    mmc.waitForSystem()
+
+    with pytest.warns(UserWarning, match="Autofocus Device is not Locked in Focus"):
+        p.add_button.click()
+    assert tb.rowCount() == 1
+    assert _get_values(tb, 0) == ["Pos000", 100.0, 200.0, 50.0, 45.0]
+    assert p._autofocus_wdg.value() == {"device_name": "Z1", "use_autofocus": True}
+
+
+def test_no_z_stage(global_mmcore: CMMCorePlus, qtbot: QtBot):
+    mmc = global_mmcore
+    mmc.unloadDevice("Z")
+
+    p = PositionTable()
+    qtbot.addWidget(p)
+
+    tb = p._table
+
+    assert tb.isColumnHidden(Z)
+
+    mmc.setXYPosition(100, 200)
+    mmc.setPosition("Z1", 45)
+    mmc.waitForSystem()
+
+    p._autofocus_wdg.setValue({"device_name": "Z1", "use_autofocus": True})
+    assert not tb.isColumnHidden(AF)
+
+    with pytest.warns(UserWarning, match="Autofocus Device is not Locked in Focus"):
+        p.add_button.click()
+    assert _get_values(tb, 0) == ["Pos000", 100.0, 200.0, 45.0]
+    assert p._autofocus_wdg.value() == {"device_name": "Z1", "use_autofocus": True}
+
+
+def test_no_xy_stage(global_mmcore: CMMCorePlus, qtbot: QtBot):
+    mmc = global_mmcore
+    mmc.unloadDevice("XY")
+
+    p = PositionTable()
+    qtbot.addWidget(p)
+
+    tb = p._table
+
+    mmc.setPosition("Z1", 45)
+    mmc.waitForSystem()
+
+    assert tb.isColumnHidden(X)
+    assert tb.isColumnHidden(Y)
+    assert tb.isColumnHidden(AF)
+
+    p._autofocus_wdg.setValue({"device_name": "Z1", "use_autofocus": True})
+    assert not tb.isColumnHidden(AF)
+    with pytest.warns(UserWarning, match="Autofocus Device is not Locked in Focus"):
+        p.add_button.click()
+    assert _get_values(tb, 0) == ["Pos000", 0.0, 45.0]
+
+
+def test_no_autofocus(global_mmcore: CMMCorePlus, qtbot: QtBot):
+    mmc = global_mmcore
+    mmc.unloadDevice("Autofocus")
+
+    p = PositionTable()
+    qtbot.addWidget(p)
+
+    assert not p._autofocus_wdg._autofocus_checkbox.isEnabled()
+    assert p._autofocus_wdg.value() == {"device_name": None, "use_autofocus": False}
+
+
+def test_set_state_with_autofocus(global_mmcore: CMMCorePlus, qtbot: QtBot):
+    p = PositionTable()
+    qtbot.addWidget(p)
+    tb = p._table
+
+    pos = {
+        "name": "Pos000",
+        "x": 10.0,
+        "y": 20.0,
+        "z": 30.0,
+        "autofocus": ("Z1", 45.0),
+        "sequence": None,
+    }
+
+    p.set_state([pos])
+
+    assert not tb.isColumnHidden(AF)
+    assert p._autofocus_wdg._autofocus_checkbox.isChecked()
+    assert p._autofocus_wdg.value() == {"device_name": "Z1", "use_autofocus": True}
+    assert _get_values(tb, 0) == ["Pos000", 10.0, 20.0, 30.0, 45.0]
+
+    assert p.value() == [pos]
