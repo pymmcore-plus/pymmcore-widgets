e6e922399a4edb8115b8fd8f33906d0f5104d793
Willi Stepp <willi.stepp@epfl.ch>
2023-09-04 10:02:14 +0200

    move example to examples

--- src/pymmcore_widgets/_mda/_stack_viewer.py
+++ src/pymmcore_widgets/_mda/_stack_viewer.py
@@ -266,29 +266,3 @@ class StackViewer(QtWidgets.QWidget):
             if i not in indices:
                 indices[i] = 0
         return indices
-
-
-if __name__ == "__main__":
-    size = 1028
-    import sys
-
-    mmcore = CMMCorePlus.instance()
-    mmcore.loadSystemConfiguration()
-
-    mmcore.setProperty("Camera", "OnCameraCCDXSize", size)
-    mmcore.setProperty("Camera", "OnCameraCCDYSize", size)
-    mmcore.setProperty("Camera", "StripeWidth", 0.7)
-    qapp = QtWidgets.QApplication(sys.argv)
-
-    sequence = MDASequence(
-        channels=({"config": "FITC", "exposure": 1}, {"config": "DAPI", "exposure": 1}),
-        time_plan={"interval": 0.5, "loops": 20},
-        axis_order="tpcz",
-    )
-
-    datastore = QLocalDataStore((40, 1, 2, size, size), mmcore=mmcore)
-    w = StackViewer(sequence=sequence, mmcore=mmcore, datastore=datastore)
-    w.show()
-
-    mmcore.run_mda(sequence)
-    qapp.exec_()
