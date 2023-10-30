24b57883ce872788a9c2b897f753a0c6c640ae12
fdrgsp <federico.gasparoli@gmail.com>
2023-10-30 13:53:42 -0400

    docs: z

--- src/pymmcore_widgets/useq_widgets/_z.py
+++ src/pymmcore_widgets/useq_widgets/_z.py
@@ -47,7 +47,7 @@ L_ARR = "\u2B05"  # LEFTWARDS BLACK ARROW
 
 
 class ZPlanWidget(QWidget):
-    """Widget representing a useq Zplan sequence."""
+    """Widget to setup a [useq.ZPlan](https://pymmcore-plus.github.io/useq-schema/schema/axes/#z-plans)."""
 
     valueChanged = Signal(object)
 
@@ -320,7 +320,7 @@ class ZPlanWidget(QWidget):
         return self._mode
 
     def setSuggestedStep(self, value: float | None) -> None:
-        """Set the suggested step size and update the button text."""
+        """Set the suggested z step size and update the button text."""
         self._suggested = value
         if value:
             self._use_suggested_btn.setText(f"{L_ARR} {value} {UM}")
@@ -330,16 +330,16 @@ class ZPlanWidget(QWidget):
             self._use_suggested_btn.hide()
 
     def suggestedStep(self) -> float | None:
-        """Return suggested step size."""
+        """Return suggested z step size."""
         return float(self._suggested) if self._suggested else None
 
     def useSuggestedStep(self) -> None:
-        """Apply the suggested step size to the step field."""
+        """Apply the suggested z step size to the step field."""
         if self._suggested:
             self.step.setValue(float(self._suggested))
 
     def value(self) -> useq.ZAboveBelow | useq.ZRangeAround | useq.ZTopBottom | None:
-        """Return the current value."""
+        """Return the current [useq.ZPlan](https://pymmcore-plus.github.io/useq-schema/schema/axes/#z-plans)."""
         if self.step.value() == 0:
             return None
 
@@ -362,7 +362,13 @@ class ZPlanWidget(QWidget):
     def setValue(
         self, value: useq.ZAboveBelow | useq.ZRangeAround | useq.ZTopBottom
     ) -> None:
-        """Set the current value."""
+        """Set the current value of the widget.
+
+        Parameters
+        ----------
+        value : useq.ZAboveBelow | useq.ZRangeAround | useq.ZTopBottom
+            One of the available [useq.ZPlan](https://pymmcore-plus.github.io/useq-schema/schema/axes/#z-plans).
+        """
         if isinstance(value, useq.ZTopBottom):
             self.top.setValue(value.top)
             self.bottom.setValue(value.bottom)
