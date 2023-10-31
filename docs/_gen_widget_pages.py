1269cf29b5e22061818bae423a2dba0788bd5d02
fdrgsp <federico.gasparoli@gmail.com>
2023-10-30 23:25:23 -0400

    docs: sort

--- docs/_gen_widget_pages.py
+++ docs/_gen_widget_pages.py
@@ -44,9 +44,9 @@ def _widget_list() -> list[tuple[str, str]]:
     import pymmcore_widgets.mda
     import pymmcore_widgets.useq_widgets
 
-    widgets = _get_widget_list(pymmcore_widgets.useq_widgets)
+    widgets = _get_widget_list(pymmcore_widgets)
+    widgets.extend(_get_widget_list(pymmcore_widgets.useq_widgets))
     widgets.extend(_get_widget_list(pymmcore_widgets.mda))
-    widgets.extend(_get_widget_list(pymmcore_widgets))
 
     return widgets
 
--- docs/_hooks.py
+++ docs/_hooks.py
@@ -13,22 +13,27 @@ def _widget_table():
     import pymmcore_widgets.mda
     import pymmcore_widgets.useq_widgets
 
-    table = _get_widget_table_list(pymmcore_widgets.useq_widgets)
-    table += _get_widget_table_list(pymmcore_widgets.mda)
-    table += _get_widget_table_list(pymmcore_widgets)
+    table = _get_widget_table_list(pymmcore_widgets)
+    table.extend(_get_widget_table_list(pymmcore_widgets.useq_widgets))
+    table.extend(_get_widget_table_list(pymmcore_widgets.mda))
 
-    return table
+    table = sorted(table, key=lambda x: x[0])
+
+    tb = ["| Widget | Description |", "| ------ | ----------- |"]
+    tb.extend(f"| [{name}][{module}.{name}] | {doc} |" for name, module, doc in table)
+
+    return "\n".join(tb)
 
 
-def _get_widget_table_list(module: str) -> list[str]:
+def _get_widget_table_list(module: str) -> list[tuple[str, str, str]]:
     from qtpy.QtWidgets import QWidget
 
-    table = ["| Widget | Description |", "| ------ | ----------- |"]
+    table = []
     for name in dir(module):
         if name.startswith("_"):
             continue
         obj = getattr(module, name)
         if isinstance(obj, type) and issubclass(obj, QWidget) and name not in EXCLUDE:
             doc = (obj.__doc__ or "").strip().splitlines()[0]
-            table.append(f"| [{name}][{module.__name__}.{name}] | {doc} |")
-    return "\n".join(table)
+            table.append((name, module.__name__, doc))
+    return table
