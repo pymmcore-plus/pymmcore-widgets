96534361995ddc93b5dbe200c6a16378f34a5ae3
fdrgsp <federico.gasparoli@gmail.com>
2023-11-07 10:15:52 -0500

    fix: update

--- src/pymmcore_widgets/_pixel_config_widget.py
+++ src/pymmcore_widgets/_pixel_config_widget.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING, Any, cast
 from pymmcore_plus import CMMCorePlus, DeviceProperty
 from qtpy.QtCore import Qt, Signal
 from qtpy.QtWidgets import (
+    QDoubleSpinBox,
     QGridLayout,
     QHBoxLayout,
     QLineEdit,
@@ -51,7 +52,7 @@ class PixelConfigurationWidget(QWidget):
         self._mmc = mmcore or CMMCorePlus.instance()
 
         self._px_table = _PixelTable()
-        self._props_selector = _PropertiesSelector(mmcore=self._mmc)
+        self._props_selector = _PropertySelector(mmcore=self._mmc)
 
         # buttons
         apply_btn = QPushButton("Apply and Close")
@@ -73,10 +74,13 @@ class PixelConfigurationWidget(QWidget):
         main_layout.addLayout(btns_layout, 2, 1)
 
         # connect signals
+        self._px_table.valueChanged.connect(self._on_px_table_value_changed)
         self._px_table._table.itemSelectionChanged.connect(
             self._on_px_table_selection_changed
         )
-        self._px_table.valueChanged.connect(self._on_px_table_value_changed)
+        self._px_table._table.itemChanged.connect(
+            lambda: self.valueChanged.emit(self.value())
+        )
         self._px_table.table().model().rowsInserted.connect(self._on_rows_inserted)
         self._props_selector.valueChanged.connect(self._on_props_selector_value_changed)
         apply_btn.clicked.connect(self._on_apply)
@@ -113,14 +117,9 @@ class PixelConfigurationWidget(QWidget):
         properties that are checked in the properties table.
         """
         self._update_current_resolutionID_data(end)
-        self._px_table.valueChanged.emit()
-        self.valueChanged.emit(self.value())
-
-    def _update_current_resolutionID_data(self, row: int) -> None:
-        """Set the data of the resolutionID item in the pixel table."""
-        data = self._props_selector._prop_table.getCheckedProperties()
-        new_item = self._px_table._table.item(row, 0)
-        new_item.setData(ID_ROLE, data)
+        # connect the valueChanged signal of the spinbox
+        wdg = cast(QDoubleSpinBox, self._px_table._table.cellWidget(end, 1))
+        wdg.valueChanged.connect(lambda: self.valueChanged.emit(self.value()))
 
     def _on_sys_config_loaded(self) -> None:
         self._px_table._remove_all()
@@ -138,24 +137,12 @@ class PixelConfigurationWidget(QWidget):
             props = self._mmc.getPixelSizeConfigData(resolutionID).dict()
             data = self._dict_to_list_of_tuples(props)
             self._px_table._table.item(row, 0).setData(ID_ROLE, data)
+            # connect the valueChanged signal of the spinbox
+            wdg = cast(QDoubleSpinBox, self._px_table._table.cellWidget(row, 1))
+            wdg.valueChanged.connect(lambda: self.valueChanged.emit(self.value()))
 
         # select first config
         self._px_table._table.selectRow(0)
-
-    def _dict_to_list_of_tuples(self, data: dict) -> list[tuple[str, str, str]]:
-        """Return a list of dev-prop-val from a 'data' dict.
-
-        Example:
-        -------
-        data = {'Objective': {'Label': 'Nikon 10X S Fluor'}}
-        returns: [('Objective', 'Label', 'Nikon 10X S Fluor')]
-        """
-        data_list: list[tuple[str, str, str]] = []
-        for key, value in data.items():
-            data_list.extend(
-                (key, sub_key, sub_value) for sub_key, sub_value in value.items()
-            )
-        return data_list
 
     def _on_px_table_selection_changed(self) -> None:
         """Update the widget when the selection in the px table changes."""
@@ -169,57 +156,6 @@ class PixelConfigurationWidget(QWidget):
 
         self._update_used_properties_and_data(items[0].row())
 
-    def _update_used_properties_and_data(self, resID_row: int | None = None) -> None:
-        """Update the properties and data for the pixel configuration.
-
-        Check the properties that are included in the specified configuration and
-        update the data of the pixel table.
-        """
-        included = [] if resID_row is None else self._get_properties_to_use(resID_row)
-        for row_props_table in range(self._props_selector._prop_table.rowCount()):
-            item = self._props_selector._prop_table.item(row_props_table, 0)
-            prop = cast(
-                DeviceProperty,
-                item.data(self._props_selector._prop_table.PROP_ROLE),
-            )
-            if (prop.device, prop.name) in included:
-                # set the property as checked
-                item.setCheckState(Qt.CheckState.Checked)
-                # update the value of the property widget
-                self._update_property_widget_value(prop, row_props_table, resID_row)
-            else:
-                item.setCheckState(Qt.CheckState.Unchecked)
-        # unchecked the 'show checked-only' checkbox if 'included' is empty
-        if not included:
-            self._props_selector._device_filters.setShowCheckedOnly(False)
-
-    def _get_properties_to_use(self, resID_row: int) -> list[tuple[str, str]]:
-        """Get the list of (device, property) to include."""
-        data = self._px_table._table.item(resID_row, 0).data(ID_ROLE)
-        included = [(d, p) for d, p, _ in data] if data is not None else []
-        # update included list with the checked properties if not already included
-        for d, p, _ in self._props_selector._prop_table.getCheckedProperties():
-            if (d, p) not in included:
-                included.append((d, p))
-        # return sorted(included, key=lambda x: x[0])
-        return included
-
-    def _update_property_widget_value(
-        self, property: DeviceProperty, row_props_table: int, resID_row: int | None
-    ) -> None:
-        """Update the value of the property widget."""
-        if resID_row is None:
-            return
-        data = self._px_table._table.item(resID_row, 0).data(ID_ROLE)
-        for dev, prop, val in data:
-            if dev == property.device and prop == property.name:
-                wdg = cast(
-                    "PropertyWidget",
-                    self._props_selector._prop_table.cellWidget(row_props_table, 1),
-                )
-                with signals_blocked(wdg._value_widget):
-                    wdg.setValue(val)
-
     def _on_props_selector_value_changed(self) -> None:
         """Update the data of the pixel table when props selection changes."""
         items = self._px_table._table.selectedItems()
@@ -228,13 +164,19 @@ class PixelConfigurationWidget(QWidget):
         self._update_current_resolutionID_data(items[0].row())
         self._update_resolutionIDs_data(items[0].row())
 
-        self.valueChanged.emit(self.value())
+        # self.valueChanged.emit(self.value())
 
     def _on_px_table_value_changed(self) -> None:
         # unchecked all properties rows if the table is empty
         if not self._px_table.value():
             self._update_used_properties_and_data()
             self._props_selector._device_filters.setShowCheckedOnly(False)
+
+    def _update_current_resolutionID_data(self, row: int) -> None:
+        """Set the data of the resolutionID item in the pixel table."""
+        data = self._props_selector._prop_table.getCheckedProperties()
+        new_item = self._px_table._table.item(row, 0)
+        new_item.setData(ID_ROLE, data)
 
     def _update_resolutionIDs_data(self, selected_resID_row: int) -> None:
         """Update the data of in all resolutionIDs if different than the data of the
@@ -268,10 +210,59 @@ class PixelConfigurationWidget(QWidget):
 
             item.setData(ID_ROLE, sorted(data, key=lambda x: x[0]))
 
+    def _update_used_properties_and_data(self, resID_row: int | None = None) -> None:
+        """Update the properties and data for the pixel configuration.
+
+        Check the properties that are included in the specified configuration and
+        update the data of the pixel table.
+        """
+        included = [] if resID_row is None else self._get_properties_to_use(resID_row)
+        for row_props_table in range(self._props_selector._prop_table.rowCount()):
+            item = self._props_selector._prop_table.item(row_props_table, 0)
+            prop = cast(
+                DeviceProperty,
+                item.data(self._props_selector._prop_table.PROP_ROLE),
+            )
+            if (prop.device, prop.name) in included:
+                # set the property as checked
+                item.setCheckState(Qt.CheckState.Checked)
+                # update the value of the property widget
+                self._update_property_widget_value(prop, row_props_table, resID_row)
+            else:
+                item.setCheckState(Qt.CheckState.Unchecked)
+        # unchecked the 'show checked-only' checkbox if 'included' is empty
+        if not included:
+            self._props_selector._device_filters.setShowCheckedOnly(False)
+
+    def _get_properties_to_use(self, resID_row: int) -> list[tuple[str, str]]:
+        """Get the list of (device, property) to include."""
+        data = self._px_table._table.item(resID_row, 0).data(ID_ROLE)
+        included = [(d, p) for d, p, _ in data] if data is not None else []
+        # update included list with the checked properties if not already included
+        for d, p, _ in self._props_selector._prop_table.getCheckedProperties():
+            if (d, p) not in included:
+                included.append((d, p))
+        return included
+
+    def _update_property_widget_value(
+        self, property: DeviceProperty, row_props_table: int, resID_row: int | None
+    ) -> None:
+        """Update the value of the property widget."""
+        if resID_row is None:
+            return
+        data = self._px_table._table.item(resID_row, 0).data(ID_ROLE)
+        for dev, prop, val in data:
+            if dev == property.device and prop == property.name:
+                wdg = cast(
+                    "PropertyWidget",
+                    self._props_selector._prop_table.cellWidget(row_props_table, 1),
+                )
+                with signals_blocked(wdg._value_widget):
+                    wdg.setValue(val)
+
     def _on_apply(self) -> None:
         """Update the current pixel size configurations."""
         # check if there are errors in the pixel configurations
-
         if self._check_for_errors():
             return
 
@@ -322,6 +313,21 @@ class PixelConfigurationWidget(QWidget):
         )
         return bool(response == QMessageBox.StandardButton.Close)
 
+    def _dict_to_list_of_tuples(self, data: dict) -> list[tuple[str, str, str]]:
+        """Return a list of dev-prop-val from a 'data' dict.
+
+        Example:
+        -------
+        data = {'Objective': {'Label': 'Nikon 10X S Fluor'}}
+        returns: [('Objective', 'Label', 'Nikon 10X S Fluor')]
+        """
+        data_list: list[tuple[str, str, str]] = []
+        for key, value in data.items():
+            data_list.extend(
+                (key, sub_key, sub_value) for sub_key, sub_value in value.items()
+            )
+        return data_list
+
 
 class _PixelTable(DataTableWidget):
     ID = TextColumn(key=ID, header="pixel configuration name", is_row_selector=False)
@@ -337,7 +343,7 @@ class _PixelTable(DataTableWidget):
         self._toolbar.actions()[2].setVisible(False)  # separator
 
 
-class _PropertiesSelector(QWidget):
+class _PropertySelector(QWidget):
     valueChanged = Signal(object)
 
     def __init__(
