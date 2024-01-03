11cda748ebd0c40730bfd006994f878b675c8057
Willi L Stepp <willi.stepp@epfl.ch>
2024-01-03 17:55:40 +0100

    tests passing except prop_widget

--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -2,7 +2,7 @@ import numpy as np
 from pymmcore_plus import CMMCorePlus
 from useq import MDAEvent, MDASequence
 
-from pymmcore_widgets._mda._datastore import QLocalDataStore
+from pymmcore_widgets._mda._datastore import QOMEZarrDatastore
 
 sequence = MDASequence(
     channels=[{"config": "DAPI", "exposure": 10}],
@@ -13,58 +13,15 @@ sequence = MDASequence(
 def test_reception(qtbot):
     mmcore = CMMCorePlus.instance()
 
-    datastore = QLocalDataStore(
-        shape=[3, 1, 1, 512, 512], dtype=np.uint16, mmcore=mmcore
-    )
+    datastore = QOMEZarrDatastore()
+    mmcore.mda.events.frameReady.connect(datastore.frameReady)
+    mmcore.mda.events.sequenceFinished.connect(datastore.sequenceFinished)
+    mmcore.mda.events.sequenceStarted.connect(datastore.sequenceStarted)
 
     with qtbot.waitSignal(datastore.frame_ready, timeout=5000):
         mmcore.run_mda(sequence, block=False)
     with qtbot.waitSignal(datastore.frame_ready, timeout=5000):
         pass
 
-    assert datastore.get_frame([0, 0, 0]).flatten()[0] != 0
+    assert datastore.get_frame(MDAEvent(index={"c":0, "t": 0})).flatten()[0] != 0
     qtbot.wait(1000)
-
-
-def test_index_completion():
-    mmcore = CMMCorePlus.instance()
-    datastore = QLocalDataStore(
-        shape=[10, 1, 1, 512, 512], dtype=np.uint16, mmcore=mmcore
-    )
-    min_indices = {"t": 0, "c": 0, "z": 0}
-    compl_indices = datastore.complement_indices(MDAEvent(index={"t": 0, "c": 0}))
-    assert min_indices == compl_indices
-
-
-def test_shape_correction():
-    mmcore = CMMCorePlus.instance()
-
-    datastore = QLocalDataStore(
-        shape=[3, 1, 1, 512, 512], dtype=np.uint16, mmcore=mmcore
-    )
-    index = {"t": 3, "c": 0, "z": 0}
-    datastore.correct_shape(index)
-    # time domain should double, think about if this makes actually sense...
-    assert datastore.array.shape == (6, 1, 1, 512, 512)
-
-    datastore = QLocalDataStore(
-        shape=[3, 1, 1, 512, 512], dtype=np.uint16, mmcore=mmcore
-    )
-    index = {"t": 1, "c": 1, "z": 0}
-    datastore.correct_shape(index)
-    assert datastore.array.shape == (3, 1, 2, 512, 512)
-
-    datastore = QLocalDataStore(
-        shape=[3, 1, 1, 512, 512], dtype=np.uint16, mmcore=mmcore
-    )
-    index = {"t": 2, "c": 0, "z": 5}
-    datastore.correct_shape(index)
-    assert datastore.array.shape == (3, 6, 1, 512, 512)
-
-    # Compound
-    datastore = QLocalDataStore(
-        shape=[3, 1, 1, 512, 512], dtype=np.uint16, mmcore=mmcore
-    )
-    index = {"t": 4, "c": 1, "z": 5}
-    datastore.correct_shape(index)
-    assert datastore.array.shape == (6, 6, 2, 512, 512)
