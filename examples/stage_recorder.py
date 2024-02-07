6be1354edac71e111e5faa796077258f73e2ca46
fdrgsp <federico.gasparoli@gmail.com>
2024-02-07 15:39:52 -0500

    feat: wip add Rectangle or Markers

--- examples/stage_recorder.py
+++ examples/stage_recorder.py
@@ -1,5 +1,6 @@
 """Example usage of the SnapButton class."""
 
+import useq
 from pymmcore_plus import CMMCorePlus
 
 # from qtpy.QtWidgets import QApplication
@@ -13,4 +14,26 @@ mmc.loadSystemConfiguration()
 rec = StageRecorder()
 rec.show()
 
+seq = useq.MDASequence(
+    channels=["FITC"],
+    grid_plan=useq.RandomPoints(
+        num_points=10,
+        max_width=-5000,
+        max_height=5000,
+        shape="rectangle",
+        fov_width=mmc.getImageWidth(),
+        fov_height=mmc.getImageHeight(),
+        allow_overlap=False,
+    ),
+)
+
 # app.exec_()
+
+
+# mmc.state(
+#     devices=False,
+#     image=False,
+#     config_groups=False,
+#     position=True,
+#     cached=False
+# )
--- src/pymmcore_widgets/_stage_recorder.py
+++ src/pymmcore_widgets/_stage_recorder.py
@@ -1,36 +1,36 @@
 from typing import Optional, Tuple
 
 import numpy as np
+from fonticon_mdi6 import MDI6
 from pymmcore_plus import CMMCorePlus
+from qtpy.QtCore import QSize, Qt
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QVBoxLayout, QWidget
-from skimage.transform import resize
-from useq import MDAEvent
+from superqt.fonticon import icon
 from vispy import scene
-from vispy.visuals.transforms import STTransform
+from vispy.scene.visuals import Markers, Rectangle
+
+BTN_SIZE = (60, 40)
+W = "white"
 
 
 class StageRecorder(QWidget):
     """A stage positions viewer widget."""
 
     def __init__(
-        self, parent: Optional[QWidget] = None, *, mmcore: Optional[CMMCorePlus] = None
+        self,
+        parent: Optional[QWidget] = None,
+        *,
+        mmcore: Optional[CMMCorePlus] = None,
+        show_fov: bool = True,
     ) -> None:
         super().__init__(parent)
 
+        self._show_fov = show_fov
+
         self._mmc = mmcore or CMMCorePlus.instance()
 
-        self._preview_visuals: Tuple = ()
-        self._mda_visuals: Tuple = ()
-        self._mda_visuals_list: list = []
-
-        self._x_range: Tuple = ()
-        self._y_range: Tuple = ()
-
-        self._create_gui()
-
-        self._mmc.events.imageSnapped.connect(self._on_image_snapped)
-
-    def _create_gui(self) -> None:
+        self._visited_positions: list[tuple[float, float]] = []
+        self._fov_max: tuple[int, int] = (1, 1)
 
         layout = QVBoxLayout()
         layout.setSpacing(0)
@@ -49,103 +49,104 @@ class StageRecorder(QWidget):
         btns_layout.setContentsMargins(5, 5, 5, 5)
         btns.setLayout(btns_layout)
         # clear button
-        self._clear_btn = QPushButton(text="Clear")
+        self._clear_btn = QPushButton()
+        self._clear_btn.setToolTip("Clear")
         self._clear_btn.clicked.connect(self._clear)
+        self._clear_btn.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self._clear_btn.setIcon(icon(MDI6.close_box_outline))
+        self._clear_btn.setIconSize(QSize(25, 25))
+        self._clear_btn.setFixedSize(*BTN_SIZE)
         # reset view button
-        self._reset_view_btn = QPushButton(text="Reset View")
+        self._reset_view_btn = QPushButton()
+        self._reset_view_btn.setToolTip("Reset View")
         self._reset_view_btn.clicked.connect(self._reset_view)
+        self._reset_view_btn.setFocusPolicy(Qt.FocusPolicy.NoFocus)
+        self._reset_view_btn.setIcon(icon(MDI6.home_outline))
+        self._reset_view_btn.setIconSize(QSize(25, 25))
+        self._reset_view_btn.setFixedSize(*BTN_SIZE)
+        # add buttons to layout
+        btns_layout.addStretch(1)
         btns_layout.addWidget(self._clear_btn)
         btns_layout.addWidget(self._reset_view_btn)
 
         layout.addWidget(btns)
 
+        self._mmc.events.imageSnapped.connect(self._on_image_snapped)
+
+    @property
+    def show_fov(self) -> bool:
+        return self._show_fov
+
+    @show_fov.setter
+    def show_fov(self, value: bool) -> None:
+        self._show_fov = value
+
     def _on_image_snapped(self) -> None:
-        self._add_preview_to_viewer(self._mmc.getImage())
+        # get current position (maybe find a different way to get the position)
+        x, y = self._mmc.getXPosition(), self._mmc.getYPosition()
 
-    def _add_preview_to_viewer(self, img: np.ndarray) -> None:
+        # set the max fov depending on the image size
+        self._set_max_fov()
 
-        _, _, w, h = self._mmc.getROI()
-
-        _width = w / 2
-        _height = h / 2
-
-        scaled = resize(img, (_width, _height))
-
-        scaled_8bit = (scaled / scaled.max()) * 255
-        scaled_8bit = np.uint8(scaled_8bit)
-
-        x = self._mmc.getXPosition()
-        y = self._mmc.getYPosition()
-
-        if len(self._preview_visuals) > 0:
-            _, preview, _, _ = self._preview_visuals
-            preview.set_data(scaled_8bit)
-        else:
-            preview = scene.visuals.Image(
-                scaled_8bit, cmap="grays", parent=self.view.scene
-            )
-
-        self._preview_visuals = ("preview", preview, x, y)
-
-        preview.transform = STTransform(translate=(x, y))
-
-        self._x_range = (x, (x + _width))
-        self._y_range = (y, (y + _width))
-
-    def _add_from_mda(self, img: np.ndarray, event: MDAEvent) -> None:
-
-        _, _, w, h = self._mmc.getROI()
-
-        # _width = w / 2
-        # _height = h / 2
-
-        _width = w
-        _height = h
-
-        # scaled = resize(img, (_width, _height))
-
-        # scaled_8bit = (scaled / scaled.max()) * 255
-        # scaled_8bit = np.uint8(scaled_8bit)
-
-        scaled_8bit = img
-
-        x = event.x_pos
-        y = event.y_pos
-
-        if len(self._mda_visuals) > 0:
-
-            _, mda, _x, _y = self._mda_visuals
-
-            if x == _x and y == _y:
-                mda.set_data(scaled_8bit)
-            else:
-                mda = scene.visuals.Image(
-                    scaled_8bit, cmap="grays", parent=self.view.scene
-                )
-                self._mda_visuals_list.append(mda)
-        else:
-            mda = scene.visuals.Image(scaled_8bit, cmap="grays", parent=self.view.scene)
-
-            self._mda_visuals_list.append(mda)
-
-        self._mda_visuals = ("mda", mda, x, y)
-
-        mda.transform = STTransform(translate=(x, y))
-
-        self._x_range = (x, (x + _width))  # type: ignore
-        self._y_range = (y, (y + _height))  # type: ignore
-
-    def _clear(self) -> None:
-        if not self._preview_visuals or not self._mda_visuals_list:
+        # return if the position is already visited
+        if (x, y) in self._visited_positions:
             return
 
-        _, img, _, _ = self._preview_visuals
-        img.parent = None
-        self._preview_visuals = ()
+        self._visited_positions.append((x, y))
+        self._draw_fov(x, y, self._mmc.getImageWidth(), self._mmc.getImageHeight())
+        self._reset_view()
 
-        for vis in self._mda_visuals_list:
-            vis.parent = None
-        self._mda_visuals_list.clear()
+    def _draw_fov(self, x: float, y: float, width: int, height: int) -> None:
+        """Draw a the position on the canvas."""
+        if self._show_fov:
+            # draw the position as a fov around the (x, y) position coordinates
+            fov = Rectangle(center=(x, y), width=width, height=height, border_color=W)
+        else:
+            # draw the (x, y) position as a point
+            fov = Markers(pos=np.array([[x, y]]), edge_color=W, face_color=W)
+        self.view.add(fov)
+
+    def _get_edges_from_visited_positions(
+        self,
+    ) -> Tuple[Tuple[float, float], Tuple[float, float]]:
+        x = [pos[0] for pos in self._visited_positions]
+        y = [pos[1] for pos in self._visited_positions]
+
+        # take in consideration the max fov
+        x_min, x_max = min(x), max(x)
+        y_min, y_max = min(y), max(y)
+        return (
+            (x_min - self._fov_max[0], x_max + self._fov_max[0]),
+            (y_min - self._fov_max[1], y_max + self._fov_max[1]),
+        )
+
+    def _set_max_fov(self) -> None:
+        """Set the max fov based on the image size.
+
+        The max size is stored in self._fov_max so that if during the session the image
+        size changes, the max fov will be updated and the view will be properly reset.
+        """
+        width = self._mmc.getImageWidth()
+        height = self._mmc.getImageHeight()
+
+        current_width_max, current_height_max = self._fov_max
+        self._fov_max = (max(width, current_width_max), max(height, current_height_max))
+
+    def _clear(self) -> None:
+        # clear visited position list
+        self._visited_positions.clear()
+
+        # remove markers
+        for child in reversed(self.view.scene.children):
+            # if isinstance(child, (Markers, Rectangle)):
+            if isinstance(child, (Rectangle)):
+                child.parent = None
+
+        self._reset_view()
 
     def _reset_view(self) -> None:
-        self.view.camera.set_range(x=self._x_range, y=self._y_range)
+        if not self._visited_positions:
+            self.view.camera.set_range()
+            return
+        (x_min, x_max), (y_min, y_max) = self._get_edges_from_visited_positions()
+        self.view.camera.set_range(x=(x_min, x_max), y=(y_min, y_max))
