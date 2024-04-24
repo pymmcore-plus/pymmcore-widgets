ef426558b7aff8232a237914b7e1f37cad44c1d7
fdrgsp <federico.gasparoli@gmail.com>
2024-04-24 14:39:32 -0400

    fix: reoreder methods

--- src/pymmcore_widgets/_camera_roi_widget.py
+++ src/pymmcore_widgets/_camera_roi_widget.py
@@ -221,12 +221,13 @@ class CameraRoiWidget(_CameraRoiGUI):
 
         self._cameras: dict[str, CameraInfo] = {}
 
-        # connections
+        # core connections
         self._mmc.events.systemConfigurationLoaded.connect(self._on_sys_cfg_loaded)
         self._mmc.events.pixelSizeChanged.connect(self._update_lbl_info)
         self._mmc.events.roiSet.connect(self._on_roi_set)
         self._mmc.events.propertyChanged.connect(self._on_property_changed)
 
+        # widget connections
         self.camera_combo.currentTextChanged.connect(self._on_camera_changed)
         self.camera_roi_combo.currentTextChanged.connect(self._on_crop_roi_mode_change)
         self.center_checkbox.toggled.connect(self._on_center_checkbox)
@@ -253,18 +254,7 @@ class CameraRoiWidget(_CameraRoiGUI):
         self._mmc.events.pixelSizeChanged.disconnect(self._update_lbl_info)
         self._mmc.events.roiSet.disconnect(self._on_roi_set)
 
-    def _get_loaded_cameras(self) -> list[str]:
-        """Get the list of loaded cameras.
-
-        It excludes the Micro-Manager Multi Camera Utilities adapter.
-        """
-        cameras: list[str] = []
-        for cam in list(self._mmc.getLoadedDevicesOfType(DeviceType.Camera)):
-            props = self._mmc.getDevicePropertyNames(cam)
-            if bool([x for x in props if "Physical Camera" in x]):
-                continue
-            cameras.append(cam)
-        return cameras
+    # ________________________________CORE CONNECTIONS________________________________
 
     def _on_sys_cfg_loaded(self) -> None:
         cameras = self._get_loaded_cameras()
@@ -315,105 +305,6 @@ class CameraRoiWidget(_CameraRoiGUI):
             return
         # show auto snap checkbox only if the selected camera is the core active camera
         self.snap_checkbox.show() if value == self.camera else self.snap_checkbox.hide()
-
-    def _store_camera_info(self, cameras: list[str]) -> None:
-        """Store the camera information in the `_cameras` dict."""
-        self._cameras.clear()
-        for camera in cameras:
-            x, y, width, height = self._mmc.getROI(camera)
-            self._cameras[camera] = CameraInfo(
-                pixel_width=width,
-                pixel_height=height,
-                crop_mode=FULL,
-                roi=ROI(x, y, width, height, True),
-            )
-
-    def _prepare_roi_combo_items(self, camera: str) -> list[str]:
-        """Prepare the ROI combo items that will be displayed in the combo box."""
-        camera_roi = self._cameras[camera]
-        items = [FULL, CUSTOM_ROI]
-        options = [8, 6, 4, 2]
-        for val in options:
-            width = round(camera_roi.pixel_width / val)
-            height = round(camera_roi.pixel_height / val)
-            items.append(f"{width} x {height}")
-        return items
-
-    def _reset_crop_mode_combo(self) -> None:
-        """Reset the crop mode combo with the selected camera options."""
-        self.camera_roi_combo.clear()
-        items = self._prepare_roi_combo_items(self.camera)
-        self.camera_roi_combo.addItems(items)
-
-    def _on_camera_changed(self, camera: str) -> None:
-        """Update the ROI When the camera combo box changes."""
-        self._update_roi_values()
-        self.camera_roi_combo.setCurrentText(self._cameras[camera].crop_mode)
-        self._update_lbl_info()
-
-        # show auto snap checkbox only if the selected camera is the core active camera
-        if self._mmc.getCameraDevice() == camera:
-            self.snap_checkbox.show()
-        else:
-            self.snap_checkbox.hide()
-
-    def _update_roi_values(self, roi: ROI | None = None) -> None:
-        """Set the ROI values for the specified camera."""
-        roi = roi or self._cameras[self.camera].roi
-        # reset the max values for start_x, start_y spinboxes. The max values should be
-        # the pixel_width - roi_width and pixel_height - roi_height
-        self.start_x.setMaximum(self._cameras[self.camera].pixel_width - roi.w)
-        self.start_y.setMaximum(self._cameras[self.camera].pixel_height - roi.h)
-        # reset the max values for the roi width and height spinboxes
-        self.roi_width.setMaximum(self._cameras[self.camera].pixel_width)
-        self.roi_height.setMaximum(self._cameras[self.camera].pixel_height)
-        # set the start_x, start_y values
-        self.start_x.setValue(roi.x)
-        self.start_y.setValue(roi.y)
-        # set the roi width, height values
-        self.roi_width.setValue(roi.w)
-        self.roi_height.setValue(roi.h)
-        # set the center checkbox state
-        self.center_checkbox.setChecked(roi.centered)
-
-    def _update_lbl_info(self) -> None:
-        """Update the info label with the current ROI information."""
-        start_x, start_y, width, height = self._get_roi_values()
-
-        px_size = self._mmc.getPixelSizeUm() or 0
-
-        width_um = width * px_size
-        height_um = height * px_size
-        text = f"Size: {width} px * {height} px [{width_um} µm * {height_um} µm]"
-
-        self.lbl_info.setText(text)
-
-        if self._mmc.getROI(self.camera) == [start_x, start_y, width, height]:
-            self.lbl_info.setStyleSheet("")
-        else:
-            self.lbl_info.setStyleSheet("color: magenta;")
-
-    def _get_roi_values(self) -> tuple[int, int, int, int]:
-        """Get the current ROI values for the selected camera."""
-        return (
-            self.start_x.value(),
-            self.start_y.value(),
-            self.roi_width.value(),
-            self.roi_height.value(),
-        )
-
-    def _clearROI(self) -> None:
-        """Clear the Camera ROI and reset to full chip."""
-        max_width = self._cameras[self.camera].pixel_width
-        max_height = self._cameras[self.camera].pixel_height
-        self._hide_spinbox_button(True)
-        self._mmc.setROI(self.camera, 0, 0, max_width, max_height)
-
-    def _on_crop_button_clicked(self) -> None:
-        """Handle the crop button click event."""
-        start_x, start_y, width, height = self._get_roi_values()
-        self._mmc.setROI(self.camera, start_x, start_y, width, height)
-        self._update_lbl_info()
 
     def _on_roi_set(self, camera: str, x: int, y: int, width: int, height: int) -> None:
         """Handle the ROI set event."""
@@ -470,55 +361,19 @@ class CameraRoiWidget(_CameraRoiGUI):
 
         self.roiChanged.emit(x, y, width, height, crop_mode)
 
-    def _update_unselected_camera_info(
-        self, camera: str, start_x: int, start_y: int, width: int, height: int
-    ) -> None:
-        centered = (
-            start_x == (self._cameras[camera].pixel_width - width) // 2
-            and start_y == (self._cameras[camera].pixel_height - height) // 2
-        )
-        crop_mode = self._get_updated_crop_mode(camera, start_x, start_y, width, height)
+    # ________________________________WIDGET CONNECTIONS________________________________
 
-        self._cameras[camera] = self._cameras[camera].replace(
-            crop_mode=crop_mode,
-            roi=ROI(start_x, start_y, width, height, centered),
-        )
+    def _on_camera_changed(self, camera: str) -> None:
+        """Update the ROI When the camera combo box changes."""
+        self._update_roi_values()
+        self.camera_roi_combo.setCurrentText(self._cameras[camera].crop_mode)
+        self._update_lbl_info()
 
-    def _get_updated_crop_mode(
-        self, camera: str, start_x: int, start_y: int, width: int, height: int
-    ) -> str:
-        """Get the updated crop mode based on the roi values."""
-        cam = self._cameras[camera]
-        # if the roi matches the full chip, set the mode to FULL
-        if (
-            start_x == 0
-            and start_y == 0
-            and width == cam.pixel_width
-            and height == cam.pixel_height
-        ):
-            mode = FULL
-
+        # show auto snap checkbox only if the selected camera is the core active camera
+        if self._mmc.getCameraDevice() == camera:
+            self.snap_checkbox.show()
         else:
-            # if the roi matches any of the default roi options, set the mode to that
-            # option otherwise set the mode to CUSTOM_ROI
-            mode = CUSTOM_ROI
-            roi_combo_items = self._prepare_roi_combo_items(camera)
-            for item in roi_combo_items:
-                if item in [FULL, CUSTOM_ROI]:
-                    continue
-                w, h = item.split(" x ")
-                x = (cam.pixel_width - int(w)) // 2
-                y = (cam.pixel_height - int(h)) // 2
-                if (
-                    start_x == x
-                    and start_y == y
-                    and width == int(w)
-                    and height == int(h)
-                ):
-                    mode = item
-                    break
-
-        return mode
+            self.snap_checkbox.hide()
 
     def _on_crop_roi_mode_change(self, value: str) -> None:
         """Handle the crop mode change."""
@@ -604,6 +459,158 @@ class CameraRoiWidget(_CameraRoiGUI):
             return
         self._update_start_max_value()
         self._emit_roi_changed_signal()
+
+    def _on_crop_button_clicked(self) -> None:
+        """Handle the crop button click event."""
+        start_x, start_y, width, height = self._get_roi_values()
+        self._mmc.setROI(self.camera, start_x, start_y, width, height)
+        self._update_lbl_info()
+
+    # ________________________________________________________________________________
+
+    def _get_loaded_cameras(self) -> list[str]:
+        """Get the list of loaded cameras.
+
+        It excludes the Micro-Manager Multi Camera Utilities adapter.
+        """
+        cameras: list[str] = []
+        for cam in list(self._mmc.getLoadedDevicesOfType(DeviceType.Camera)):
+            props = self._mmc.getDevicePropertyNames(cam)
+            if bool([x for x in props if "Physical Camera" in x]):
+                continue
+            cameras.append(cam)
+        return cameras
+
+    def _store_camera_info(self, cameras: list[str]) -> None:
+        """Store the camera information in the `_cameras` dict."""
+        self._cameras.clear()
+        for camera in cameras:
+            x, y, width, height = self._mmc.getROI(camera)
+            self._cameras[camera] = CameraInfo(
+                pixel_width=width,
+                pixel_height=height,
+                crop_mode=FULL,
+                roi=ROI(x, y, width, height, True),
+            )
+
+    def _reset_crop_mode_combo(self) -> None:
+        """Reset the crop mode combo with the selected camera options."""
+        self.camera_roi_combo.clear()
+        items = self._prepare_roi_combo_items(self.camera)
+        self.camera_roi_combo.addItems(items)
+
+    def _prepare_roi_combo_items(self, camera: str) -> list[str]:
+        """Prepare the ROI combo items that will be displayed in the combo box."""
+        camera_roi = self._cameras[camera]
+        items = [FULL, CUSTOM_ROI]
+        options = [8, 6, 4, 2]
+        for val in options:
+            width = round(camera_roi.pixel_width / val)
+            height = round(camera_roi.pixel_height / val)
+            items.append(f"{width} x {height}")
+        return items
+
+    def _update_lbl_info(self) -> None:
+        """Update the info label with the current ROI information."""
+        start_x, start_y, width, height = self._get_roi_values()
+
+        px_size = self._mmc.getPixelSizeUm() or 0
+
+        width_um = width * px_size
+        height_um = height * px_size
+        text = f"Size: {width} px * {height} px [{width_um} µm * {height_um} µm]"
+
+        self.lbl_info.setText(text)
+
+        if self._mmc.getROI(self.camera) == [start_x, start_y, width, height]:
+            self.lbl_info.setStyleSheet("")
+        else:
+            self.lbl_info.setStyleSheet("color: magenta;")
+
+    def _update_roi_values(self, roi: ROI | None = None) -> None:
+        """Set the ROI values for the specified camera."""
+        roi = roi or self._cameras[self.camera].roi
+        # reset the max values for start_x, start_y spinboxes. The max values should be
+        # the pixel_width - roi_width and pixel_height - roi_height
+        self.start_x.setMaximum(self._cameras[self.camera].pixel_width - roi.w)
+        self.start_y.setMaximum(self._cameras[self.camera].pixel_height - roi.h)
+        # reset the max values for the roi width and height spinboxes
+        self.roi_width.setMaximum(self._cameras[self.camera].pixel_width)
+        self.roi_height.setMaximum(self._cameras[self.camera].pixel_height)
+        # set the start_x, start_y values
+        self.start_x.setValue(roi.x)
+        self.start_y.setValue(roi.y)
+        # set the roi width, height values
+        self.roi_width.setValue(roi.w)
+        self.roi_height.setValue(roi.h)
+        # set the center checkbox state
+        self.center_checkbox.setChecked(roi.centered)
+
+    def _get_roi_values(self) -> tuple[int, int, int, int]:
+        """Get the current ROI values for the selected camera."""
+        return (
+            self.start_x.value(),
+            self.start_y.value(),
+            self.roi_width.value(),
+            self.roi_height.value(),
+        )
+
+    def _clearROI(self) -> None:
+        """Clear the Camera ROI and reset to full chip."""
+        max_width = self._cameras[self.camera].pixel_width
+        max_height = self._cameras[self.camera].pixel_height
+        self._hide_spinbox_button(True)
+        self._mmc.setROI(self.camera, 0, 0, max_width, max_height)
+
+    def _update_unselected_camera_info(
+        self, camera: str, start_x: int, start_y: int, width: int, height: int
+    ) -> None:
+        centered = (
+            start_x == (self._cameras[camera].pixel_width - width) // 2
+            and start_y == (self._cameras[camera].pixel_height - height) // 2
+        )
+        crop_mode = self._get_updated_crop_mode(camera, start_x, start_y, width, height)
+
+        self._cameras[camera] = self._cameras[camera].replace(
+            crop_mode=crop_mode,
+            roi=ROI(start_x, start_y, width, height, centered),
+        )
+
+    def _get_updated_crop_mode(
+        self, camera: str, start_x: int, start_y: int, width: int, height: int
+    ) -> str:
+        """Get the updated crop mode based on the roi values."""
+        cam = self._cameras[camera]
+        # if the roi matches the full chip, set the mode to FULL
+        if (
+            start_x == 0
+            and start_y == 0
+            and width == cam.pixel_width
+            and height == cam.pixel_height
+        ):
+            mode = FULL
+
+        else:
+            # if the roi matches any of the default roi options, set the mode to that
+            # option otherwise set the mode to CUSTOM_ROI
+            mode = CUSTOM_ROI
+            roi_combo_items = self._prepare_roi_combo_items(camera)
+            for item in roi_combo_items:
+                if item in [FULL, CUSTOM_ROI]:
+                    continue
+                w, h = item.split(" x ")
+                x = (cam.pixel_width - int(w)) // 2
+                y = (cam.pixel_height - int(h)) // 2
+                if (
+                    start_x == x
+                    and start_y == y
+                    and width == int(w)
+                    and height == int(h)
+                ):
+                    mode = item
+                    break
+
+        return mode
 
     def _emit_roi_changed_signal(self) -> None:
         """Update the camera info with the new ROI values."""
