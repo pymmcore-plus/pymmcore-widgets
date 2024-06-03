3d41c084a6692cbc4cde5cc1ae3bbf49b20efe1d
fdrgsp <federico.gasparoli@gmail.com>
2024-06-03 14:27:26 -0400

    fix: example

--- examples/hcs_wizard.py
+++ examples/hcs_wizard.py
@@ -29,7 +29,11 @@ height = mmc.getImageHeight() * mmc.getPixelSizeUm()
 
 data = HCSData(
     plate=database["standard 96 wp"],
-    wells=[Well("A1", 0, 0), Well("B2", 1, 1), Well("C3", 2, 2)],
+    wells=[
+        Well(name="A1", row=0, column=0),
+        Well(name="B2", row=1, column=1),
+        Well(name="C3", row=2, column=2),
+    ],
     mode=Center(x=0, y=0, fov_width=width, fov_height=height),
     calibration=CalibrationData(
         plate=database["standard 96 wp"],
