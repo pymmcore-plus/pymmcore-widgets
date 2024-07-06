6f3d78b910f5261a71acf17352b968e1da642f77
Talley Lambert <talley.lambert@gmail.com>
2024-07-05 21:48:14 -0400

    refactor: remove old MDA widget (#313)

--- src/pymmcore_widgets/useq_widgets/_mda_sequence.py
+++ src/pymmcore_widgets/useq_widgets/_mda_sequence.py
@@ -20,8 +20,8 @@ from qtpy.QtWidgets import (
 from superqt.utils import signals_blocked
 
 import pymmcore_widgets
-from pymmcore_widgets._mda._checkable_tabwidget_widget import CheckableTabWidget
 from pymmcore_widgets.useq_widgets._channels import ChannelTable
+from pymmcore_widgets.useq_widgets._checkable_tabwidget_widget import CheckableTabWidget
 from pymmcore_widgets.useq_widgets._grid import GridPlanWidget
 from pymmcore_widgets.useq_widgets._positions import PositionTable
 from pymmcore_widgets.useq_widgets._time import TimePlanWidget
