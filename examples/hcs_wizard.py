4138c51e4c41a0391499f6c3cfe4b3c22849087b
fdrgsp <federico.gasparoli@gmail.com>
2024-09-12 14:39:30 -0400

    fix: example

--- examples/hcs_wizard.py
+++ examples/hcs_wizard.py
@@ -1,5 +1,6 @@
 from contextlib import suppress
 
+import useq
 from pymmcore_plus import CMMCorePlus
 from qtpy.QtWidgets import QApplication
 
@@ -20,11 +21,11 @@ s = StageWidget("XY", mmcore=mmc)
 s.show()
 
 
-# plan = useq.WellPlatePlan(
-#     plate=useq.WellPlate.from_str("96-well"),
-#     a1_center_xy=(1000, 1500),
-#     rotation=0.3,
-# )
-# w.setValue(plan)
+plan = useq.WellPlatePlan(
+    plate=useq.WellPlate.from_str("96-well"),
+    a1_center_xy=(1000, 1500),
+    rotation=0.3,
+)
+w.setValue(plan)
 
 app.exec()
