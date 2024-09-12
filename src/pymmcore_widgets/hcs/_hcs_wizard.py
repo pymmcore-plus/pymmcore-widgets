159cdcc215cfb98a4ab889dd8cb21fb3fad6ca3d
Talley Lambert <talley.lambert@gmail.com>
2024-09-12 15:40:50 -0400

    fix: deselect wells with no explicit selection

--- src/pymmcore_widgets/hcs/_hcs_wizard.py
+++ src/pymmcore_widgets/hcs/_hcs_wizard.py
@@ -101,6 +101,9 @@ class HCSWizard(QWizard):
         self.plate_page.widget.setValue(value)
         self.calibration_page.widget.setValue(value)
         self.points_plan_page.widget.setValue(value.well_points_plan)
+        # if no wells are explicitly selected, deselect all wells
+        if value.selected_wells is None:
+            self.plate_page.widget._view.setSelectedIndices([])  # select None
 
     def save(self, path: str | None = None) -> None:
         """Save the current well plate plan to disk."""
