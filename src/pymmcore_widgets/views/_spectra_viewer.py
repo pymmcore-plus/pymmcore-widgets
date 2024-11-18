affdbedd4387282815f25da6567fb72515bd3963
Talley Lambert <talley.lambert@gmail.com>
2024-11-17 21:33:04 -0500

    updates

--- src/pymmcore_widgets/views/_spectra_viewer.py
+++ src/pymmcore_widgets/views/_spectra_viewer.py
@@ -1,7 +1,7 @@
 from vispy import scene
 
 from pymmcore_widgets._vispy_plot import PlotWidget
-from pymmcore_widgets.fpbase import get_fluorophore
+from pymmcore_widgets.fpbase import Spectrum, get_filter, get_fluorophore
 
 
 class SpectraViewer:
@@ -17,10 +17,17 @@ class SpectraViewer:
         self.plot.yaxis.visible = False
         self.view.add_widget(self.plot)
 
-    def add_fluorophore(self, name: str) -> None:
-        fluor = get_fluorophore(name)
-        for state in fluor.states:
-            if state.excitation_spectrum is not None:
-                self.plot.plot(state.excitation_spectrum.data, color=state.exhex)
-            if state.emission_spectrum is not None:
-                self.plot.plot(state.emission_spectrum.data, color=state.emhex)
+    def add_spectrum(self, name: str) -> None:
+        spectra: list[tuple[Spectrum, str]] = []
+        try:
+            spectra.append((get_filter(name), "#AAAAAA"))
+        except ValueError:
+            fluor = get_fluorophore(name)
+            for state in fluor.states:
+                if state.excitation_spectrum:
+                    spectra.append((state.excitation_spectrum, state.exhex))
+                if state.emission_spectrum:
+                    spectra.append((state.emission_spectrum, state.emhex))
+
+        for spectrum, color in spectra:
+            self.plot.plot(spectrum.data, color=color)
--- x.py
+++ x.py
@@ -3,7 +3,8 @@ from vispy import app
 from pymmcore_widgets.views._spectra_viewer import SpectraViewer
 
 widget = SpectraViewer()
-widget.add_fluorophore("mTurquoise")
-widget.add_fluorophore("mStayGold")
-widget.add_fluorophore("mCherry")
+widget.add_spectrum("mTurquoise")
+widget.add_spectrum("mStayGold")
+widget.add_spectrum("mCherry")
+widget.add_spectrum("Chroma ET525/50m")
 app.run()
