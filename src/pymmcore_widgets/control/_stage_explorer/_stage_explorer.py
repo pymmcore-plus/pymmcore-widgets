5290dbf63b6a46f54add6c9ebfb83e56454f0f2e
fdrgsp <federico.gasparoli@gmail.com>
2024-11-28 16:33:17 -0500

    fix: simplify StageViewer and move logic to StageExplorer

--- src/pymmcore_widgets/control/_stage_explorer/_stage_explorer.py
+++ src/pymmcore_widgets/control/_stage_explorer/_stage_explorer.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import cast
 
 import numpy as np
@@ -17,16 +18,17 @@ from qtpy.QtWidgets import (
 )
 from superqt.fonticon import icon
 from vispy.app.canvas import MouseEvent
+from vispy.scene.visuals import Markers
 
-from ._stage_viewer import StageViewer
+from ._stage_viewer import DataStore, StageViewer
 
 red = "#C33"
 green = "#3A3"
 gray = "#666"
 RESET = "Auto Reset View"
 SNAP = "Auto Snap on Double Click"
-FLIP_H = "Flip Images Horizontally"
-FLIP_V = "Flip Images Vertically"
+FLIP_X = "Flip Images Horizontally"
+FLIP_Y = "Flip Images Vertically"
 POLL_STAGE = "Poll Stage Position"
 
 
@@ -50,16 +52,19 @@ class StageExplorer(QWidget):
 
     Properties
     ----------
+    image_store : DataStore
+        Return the image store object that contains the images added to the scene and
+        their stage positions.
     auto_reset_view : bool
         A boolean property that controls whether to automatically reset the view when an
         image is added to the scene. By default, True.
     snap_on_double_click : bool
         A boolean property that controls whether to snap an image when the user
         double-clicks on the view. By default, False.
-    flip_horizontal : bool
+    flip_x : bool
         A boolean property that controls whether to flip images horizontally. By
         default, False.
-    flip_vertical : bool
+    flip_y : bool
         A boolean property that controls whether to flip images vertically. By default,
         False.
     poll_stage_position : bool
@@ -83,8 +88,15 @@ class StageExplorer(QWidget):
         self._timer_id: int | None = None
 
         # properties
+        self._auto_reset_view: bool = True
         self._snap_on_double_click: bool = False
+        self._flip_x: bool = False
+        self._flip_y: bool = False
         self._poll_stage_position: bool = False
+        self._image_store: DataStore = self._stage_viewer.image_store
+
+        # marker for stage position
+        self._stage_pos_marker: Markers | None = None
 
         # toolbar ---------------------------------------------------------------------
         toolbar = QToolBar()
@@ -94,7 +106,7 @@ class StageExplorer(QWidget):
         self._act_reset = QAction(
             icon(MDI6.fullscreen, color=green), "Reset View", self
         )
-        self._act_reset.triggered.connect(self._stage_viewer.reset_view)
+        self._act_reset.triggered.connect(self.reset_view)
         # clear action
         self._act_clear = QAction(icon(MDI6.close, color=red), "Clear View", self)
         self._act_clear.triggered.connect(self._stage_viewer.clear_scene)
@@ -108,25 +120,25 @@ class StageExplorer(QWidget):
         self._settings_btn.setMenu(menu)
         # create checkable actions for settings
         self._auto_reset_act = QAction(RESET, self, checkable=True)
-        self._auto_reset_act.setChecked(self._stage_viewer.auto_reset_view)
+        self._auto_reset_act.setChecked(self.auto_reset_view)
         self._auto_snap_act = QAction(SNAP, self, checkable=True)
         self._auto_snap_act.setChecked(self.snap_on_double_click)
-        self._flip_horizontal_act = QAction(FLIP_H, self, checkable=True)
-        self._flip_horizontal_act.setChecked(self._stage_viewer.flip_horizontal)
-        self._flip_vertical_act = QAction(FLIP_V, self, checkable=True)
-        self._flip_vertical_act.setChecked(self._stage_viewer.flip_vertical)
+        self._flip_x_act = QAction(FLIP_X, self, checkable=True)
+        self._flip_x_act.setChecked(self.flip_x)
+        self._flip_y_act = QAction(FLIP_Y, self, checkable=True)
+        self._flip_y_act.setChecked(self.flip_y)
         self._poll_stage_act = QAction(POLL_STAGE, self, checkable=True)
         # connect actions to respective slots
         self._auto_reset_act.triggered.connect(self._on_reset_view)
         self._auto_snap_act.triggered.connect(self._on_setting_checked)
-        self._flip_horizontal_act.triggered.connect(self._on_setting_checked)
-        self._flip_vertical_act.triggered.connect(self._on_setting_checked)
+        self._flip_x_act.triggered.connect(self._on_setting_checked)
+        self._flip_y_act.triggered.connect(self._on_setting_checked)
         self._poll_stage_act.triggered.connect(self._on_poll_stage)
         # add actions to the menu
         menu.addAction(self._auto_reset_act)
         menu.addAction(self._auto_snap_act)
-        menu.addAction(self._flip_horizontal_act)
-        menu.addAction(self._flip_vertical_act)
+        menu.addAction(self._flip_x_act)
+        menu.addAction(self._flip_y_act)
         menu.addAction(self._poll_stage_act)
         # stage pos label
         self._stage_pos_label = QLabel()
@@ -161,17 +173,22 @@ class StageExplorer(QWidget):
     # -----------------------------PUBLIC METHODS-------------------------------------
 
     @property
+    def image_store(self) -> DataStore:
+        """Return the image store."""
+        return self._stage_viewer.image_store
+
+    @property
     def auto_reset_view(self) -> bool:
         """Return the auto reset view property."""
-        return self._stage_viewer.auto_reset_view
+        return self._auto_reset_view
 
     @auto_reset_view.setter
     def auto_reset_view(self, value: bool) -> None:
         """Set the auto reset view property."""
-        self._stage_viewer.auto_reset_view = value
+        self._auto_reset_view = value
         self._auto_reset_act.setChecked(value)
         if value:
-            self._stage_viewer.reset_view()
+            self.reset_view()
 
     @property
     def snap_on_double_click(self) -> bool:
@@ -185,26 +202,26 @@ class StageExplorer(QWidget):
         self._auto_snap_act.setChecked(value)
 
     @property
-    def flip_horizontal(self) -> bool:
-        """Return the flip horizontal property."""
-        return self._stage_viewer.flip_horizontal
+    def flip_x(self) -> bool:
+        """Return the flip x property."""
+        return self._flip_x
 
-    @flip_horizontal.setter
-    def flip_horizontal(self, value: bool) -> None:
-        """Set the flip horizontal property."""
-        self._stage_viewer.flip_horizontal = value
-        self._flip_horizontal_act.setChecked(value)
+    @flip_x.setter
+    def flip_x(self, value: bool) -> None:
+        """Set the flip x property."""
+        self._flip_x = value
+        self._flip_x_act.setChecked(value)
 
     @property
-    def flip_vertical(self) -> bool:
-        """Return the flip vertical property."""
-        return self._stage_viewer.flip_vertical
+    def flip_y(self) -> bool:
+        """Return the flip y property."""
+        return self._flip_y
 
-    @flip_vertical.setter
-    def flip_vertical(self, value: bool) -> None:
-        """Set the flip vertical property."""
-        self._stage_viewer.flip_vertical = value
-        self._flip_vertical_act.setChecked(value)
+    @flip_y.setter
+    def flip_y(self, value: bool) -> None:
+        """Set the flip y property."""
+        self._flip_y = value
+        self._flip_y_act.setChecked(value)
 
     @property
     def poll_stage_position(self) -> bool:
@@ -218,13 +235,68 @@ class StageExplorer(QWidget):
         self._poll_stage_act.setChecked(value)
         self._on_poll_stage(value)
 
+    def add_image(
+        self,
+        img: np.ndarray,
+        x: float,
+        y: float,
+        flip_x: bool = False,
+        flip_y: bool = False,
+    ) -> None:
+        """Add an image to the scene.
+
+        The image is also added to the `image_store` DataStore which is a dictionary
+        that uses the (x, y) positions as key and the images as value.
+
+        Parameters
+        ----------
+        img : np.ndarray
+            The image to add to the scene.
+        x : float
+            The x position of the image.
+        y : float
+            The y position of the image.
+        flip_x : bool
+            Flip the image horizontally, by default False.
+        flip_y : bool
+            Flip the image vertically, by default False.
+        """
+        if flip_x:
+            img = np.flip(img, axis=1)
+        if flip_y:
+            img = np.flip(img, axis=0)
+        self._stage_viewer.add_image(img, x, y)
+        if self._auto_reset_view:
+            self.reset_view()
+
+    def reset_view(self) -> None:
+        """Recenter the view to the center of all images."""
+        # NOTE: Not using the `reset_view` method from `StageViewer` directly because we
+        # also need to consider the stage position marker.
+
+        min_x, max_x, min_y, max_y = self._stage_viewer._get_boundaries()
+
+        # consider the stage position marker if present
+        if self._stage_pos_marker is not None:
+            x, y = self._get_stage_marker_position()
+            if x is not None and y is not None:
+                min_x = int(x) if min_x is None else min(min_x, int(x))
+                max_x = int(x) if max_x is None else max(max_x, int(x))
+                min_y = int(y) if min_y is None else min(min_y, int(y))
+                max_y = int(y) if max_y is None else max(max_y, int(y))
+
+        if any(val is None for val in (min_x, max_x, min_y, max_y)):
+            return
+
+        self._stage_viewer.view.camera.set_range(x=(min_x, max_x), y=(min_y, max_y))
+
     # -----------------------------PRIVATE METHODS------------------------------------
 
     def _on_reset_view(self, checked: bool) -> None:
         """Set the auto reset view property based on the state of the action."""
-        self._stage_viewer.auto_reset_view = checked
+        self._auto_reset_view = checked
         if checked:
-            self._stage_viewer.reset_view()
+            self.reset_view()
 
     def _on_poll_stage(self, checked: bool) -> None:
         """Set the poll stage position property based on the state of the action."""
@@ -234,6 +306,10 @@ class StageExplorer(QWidget):
             self.killTimer(self._timer_id)
             self._timer_id = None
             self._stage_pos_label.setText("")
+            # delete markers
+            if self._stage_pos_marker is not None:
+                self._stage_pos_marker.parent = None
+                self._stage_pos_marker = None
 
     def timerEvent(self, event: QTimerEvent) -> None:
         """Poll the stage position."""
@@ -242,16 +318,57 @@ class StageExplorer(QWidget):
         x, y = self._mmc.getXYPosition()
         self._stage_pos_label.setText(f"X: {x:.2f} Y: {y:.2f}")
 
+        # add stage marker if not yet present
+        if self._stage_pos_marker is None:
+            self._stage_pos_marker = Markers(
+                parent=self._stage_viewer.view.scene, antialias=True
+            )
+            self._stage_pos_marker.set_gl_state(depth_test=False)
+        # update stage marker position
+        self._stage_pos_marker.set_data(
+            symbol="cross_lines",
+            edge_color="green",
+            size=50,
+            edge_width=7,
+            pos=np.array([[x, y]]),
+        )
+
+        if self._auto_reset_view:
+            self.reset_view()
+
     def _on_setting_checked(self, checked: bool) -> None:
         """Update the stage viewer settings based on the state of the action."""
         action_map = {
             self._auto_snap_act: "snap_on_double_click",
-            self._flip_horizontal_act: "flip_horizontal",
-            self._flip_vertical_act: "flip_vertical",
+            self._flip_x_act: "flip_x",
+            self._flip_y_act: "flip_y",
         }
         sender = cast(QAction, self.sender())
+
         if value := action_map.get(sender):
+            print(sender.text(), value, action_map.get(sender))
             setattr(self, value, checked)
+
+    def _get_stage_marker_position(self) -> tuple[float | None, float | None]:
+        """Return the stage marker position.
+
+        NOTE: the only way to get the a vispy Markers position is to use their private
+        _data['a_position'] attribute. In this method we first check if the private
+        attribute is available, if not we return the current stage position.
+        """
+        if self._stage_pos_marker is None:
+            return None, None
+        try:
+            # NOTE: _data is a numpy array
+            x, y, _ = self._stage_pos_marker._data["a_position"][0]
+        except (AttributeError, IndexError, TypeError, ValueError):
+            warnings.warn(
+                "Could not access the '_data' attribute of vispy Markers object. "
+                "Returning the current stage position.",
+                stacklevel=2,
+            )
+            x, y = self._mmc.getXYPosition()
+        return x, y
 
     def _on_pixel_size_changed(self, value: float) -> None:
         """Clear the scene when the pixel size changes."""
@@ -266,14 +383,14 @@ class StageExplorer(QWidget):
         # get the current stage position
         x, y = self._mmc.getXYPosition()
         # move the coordinates to the center of the image
-        self._stage_viewer.add_image(img, x, y)
+        self.add_image(img, x, y, self.flip_x, self.flip_y)
 
     def _on_frame_ready(self, image: np.ndarray, event: useq.MDAEvent) -> None:
         """Add the image to the scene when frameReady event is emitted."""
         # TODO: better handle z stack (e.g. max projection?)
         x = event.x_pos if event.x_pos is not None else self._mmc.getXPosition()
         y = event.y_pos if event.y_pos is not None else self._mmc.getYPosition()
-        self._stage_viewer.add_image(image, x, y)
+        self.add_image(image, x, y, self.flip_x, self.flip_y)
 
     def _move_to_clicked_position(self, event: MouseEvent) -> None:
         """Move the stage to the clicked position."""
--- src/pymmcore_widgets/control/_stage_explorer/_stage_viewer.py
+++ src/pymmcore_widgets/control/_stage_explorer/_stage_viewer.py
@@ -1,3 +1,4 @@
+from collections.abc import Iterator
 from typing import Optional, cast
 
 import numpy as np
@@ -22,6 +23,10 @@ class DataStore:
     def __init__(self) -> None:
         self.store: dict[tuple[float, float], np.ndarray] = {}
 
+    def clear(self) -> None:
+        """Clear the store."""
+        self.store.clear()
+
     def add_image(self, position: tuple[float, float], image: np.ndarray) -> None:
         """Add an image to the store."""
         self.store[position] = image
@@ -29,6 +34,9 @@ class DataStore:
     def get_image(self, position: tuple[float, float]) -> np.ndarray | None:
         """Get an image from the store."""
         return self.store.get(position, None)
+
+    def __iter__(self) -> Iterator[tuple[tuple[float, float], np.ndarray]]:
+        return iter(self.store.items())
 
 
 class StageViewer(QWidget):
@@ -50,17 +58,8 @@ class StageViewer(QWidget):
     image_store : DataStore
         Return the image store object that contains the images added to the scene and
         their stage positions.
-    auto_reset_view : bool
-        A boolean property that controls whether to automatically reset the view when an
-        image is added to the scene. By default, True.
     pixel_size : float
         The pixel size in micrometers. By default, 1.0.
-    flip_horizontal : bool
-        A boolean property that controls whether to flip images horizontally. By
-        default, False.
-    flip_vertical : bool
-        A boolean property that controls whether to flip images vertically. By default,
-        False.
     """
 
     scaleChanged = Signal(int)
@@ -73,10 +72,7 @@ class StageViewer(QWidget):
 
         # properties
         self._image_store: DataStore = DataStore()
-        self._auto_reset_view: bool = True
         self._pixel_size: float = 1.0
-        self._flip_horizontal: bool = False
-        self._flip_vertical: bool = False
 
         self.canvas = scene.SceneCanvas(keys="interactive", show=True)
         self.view = cast(ViewBox, self.canvas.central_widget.add_view())
@@ -87,8 +83,7 @@ class StageViewer(QWidget):
         main_layout.setContentsMargins(0, 0, 0, 0)
         main_layout.addWidget(self.canvas.native)
 
-        # connections
-        # this is to check if the scale has changed and update the scene accordingly
+        # connections (if the scale has changed, update the scene accordingly)
         self.canvas.events.draw.connect(self._on_draw_event)
 
     # --------------------PUBLIC METHODS--------------------
@@ -97,16 +92,6 @@ class StageViewer(QWidget):
     def image_store(self) -> DataStore:
         """Return the image store."""
         return self._image_store
-
-    @property
-    def auto_reset_view(self) -> bool:
-        """Return the auto reset view property."""
-        return self._auto_reset_view
-
-    @auto_reset_view.setter
-    def auto_reset_view(self, value: bool) -> None:
-        """Set the auto reset view property."""
-        self._auto_reset_view = value
 
     @property
     def pixel_size(self) -> float:
@@ -118,33 +103,21 @@ class StageViewer(QWidget):
         """Set the pixel size."""
         self._pixel_size = value
 
-    @property
-    def flip_horizontal(self) -> bool:
-        """Return the flip horizontally setting."""
-        return self._flip_horizontal
-
-    @flip_horizontal.setter
-    def flip_horizontal(self, value: bool) -> None:
-        """Set the flip horizontally setting."""
-        self._flip_horizontal = value
-
-    @property
-    def flip_vertical(self) -> bool:
-        """Return the flip vertically setting."""
-        return self._flip_vertical
-
-    @flip_vertical.setter
-    def flip_vertical(self, value: bool) -> None:
-        """Set the flip vertically setting."""
-        self._flip_vertical = value
-
     def add_image(self, img: np.ndarray, x: float, y: float) -> None:
-        """Add an image to the scene and to the image_store."""
-        # flip the image if needed
-        if self._flip_horizontal:
-            img = np.fliplr(img)
-        if self._flip_vertical:
-            img = np.flipud(img)
+        """Add an image to the scene.
+
+        The image is also added to the `image_store` DataStore which is a dictionary
+        that uses the (x, y) positions as key and the images as value.
+
+        Parameters
+        ----------
+        img : np.ndarray
+            The image to add to the scene.
+        x : float
+            The x position of the image.
+        y : float
+            The y position of the image.
+        """
         # move the coordinates to the center of the image
         h, w = np.array(img.shape)
         x, y = round(x - w / 2 * self.pixel_size), round(y - h / 2 * self.pixel_size)
@@ -156,30 +129,26 @@ class StageViewer(QWidget):
         img = img[::scale, ::scale]
         frame = Image(img, cmap="grays", parent=self.view.scene, clim="auto")
         # keep the added image on top of the others
-        order = min(child.order for child in self.view.scene.children) + -1
-        frame.order = order
+        frame.order = min(child.order for child in self._get_images()) - 1
         frame.transform = scene.STTransform(
             scale=(scale * self.pixel_size, scale * self.pixel_size), translate=(x, y)
         )
-        if self._auto_reset_view:
-            self.reset_view()
 
     def update_by_scale(self, scale: int) -> None:
         """Update the images in the scene based on scale and pixel size."""
-        for child in self.view.scene.children:
-            if isinstance(child, Image):
-                x, y = child.transform.translate[:2]
-                img = self._image_store.get_image((x, y))
-                if img is None:
-                    continue
-                img_scaled = img[::scale, ::scale]
-                # update the image data
-                child.set_data(img_scaled)
-                # update the scale
-                child.transform.scale = (
-                    scale * self.pixel_size,
-                    scale * self.pixel_size,
-                )
+        for child in self._get_images():
+            x, y = child.transform.translate[:2]
+            img = self._image_store.get_image((x, y))
+            if img is None:
+                continue
+            img_scaled = img[::scale, ::scale]
+            # update the image data
+            child.set_data(img_scaled)
+            # update the scale
+            child.transform.scale = (
+                scale * self.pixel_size,
+                scale * self.pixel_size,
+            )
 
     def get_scale(self) -> int:
         """Return the scale based on the zoom level."""
@@ -195,7 +164,7 @@ class StageViewer(QWidget):
 
     def clear_scene(self) -> None:
         """Clear the scene."""
-        self._image_store.store.clear()
+        self._image_store.clear()
 
         for child in reversed(self.view.scene.children):
             if isinstance(child, Image):
@@ -203,26 +172,28 @@ class StageViewer(QWidget):
 
     def reset_view(self) -> None:
         """Recenter the view to the center of all images."""
+        min_x, max_x, min_y, max_y = self._get_boundaries()
+        if any(val is None for val in (min_x, max_x, min_y, max_y)):
+            return
+        self.view.camera.set_range(x=(min_x, max_x), y=(min_y, max_y))
+
+    # --------------------PRIVATE METHODS--------------------
+
+    def _get_boundaries(self) -> tuple[int | None, int | None, int | None, int | None]:
+        """Return the boundaries of the images in the scene."""
+        # get the max and min (x, y) values from _store_images
         min_x: int | None = None
         max_x: int | None = None
         min_y: int | None = None
         max_y: int | None = None
-
-        # get the max and min (x, y) values from _store_images
-        for (x, y), img in self._image_store.store.items():
+        for (x, y), img in self._image_store:
             height, width = np.array(img.shape) * self.pixel_size
             x, y = round(x), round(y)
             min_x = x if min_x is None else min(min_x, x)
             max_x = x + width if max_x is None else max(max_x, x + width)
             min_y = y if min_y is None else min(min_y, y)
             max_y = y + height if max_y is None else max(max_y, y + height)
-
-        if min_x is None or max_x is None or min_y is None or max_y is None:
-            return
-
-        self.view.camera.set_range(x=(min_x, max_x), y=(min_y, max_y))
-
-    # --------------------PRIVATE METHODS--------------------
+        return min_x, max_x, min_y, max_y
 
     def _on_draw_event(self, event: MouseEvent) -> None:
         """Handle the draw event.
@@ -235,3 +206,7 @@ class StageViewer(QWidget):
         self._current_scale = scale
         self.update_by_scale(scale)
         self.scaleChanged.emit(scale)
+
+    def _get_images(self) -> list[Image]:
+        """Return a list of images in the scene."""
+        return [child for child in self.view.scene.children if isinstance(child, Image)]
