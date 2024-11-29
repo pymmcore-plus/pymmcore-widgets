232c753a1e68d8eccaee302f4a663572b619a93f
fdrgsp <federico.gasparoli@gmail.com>
2024-11-29 11:41:25 -0500

    fix: example

--- examples/stage_explorer_widget.py
+++ examples/stage_explorer_widget.py
@@ -1,46 +1,21 @@
 from pymmcore_plus import CMMCorePlus
-from qtpy.QtWidgets import (
-    QApplication,
-    QHBoxLayout,
-    QVBoxLayout,
-    QWidget,
-)
+from qtpy.QtWidgets import QApplication
 
 from pymmcore_widgets import StageWidget
 from pymmcore_widgets.control import StageExplorer
 
+app = QApplication([])
 
-class Explorer(QWidget):
-    """Example that shows the use of the `StageExplorer` widget."""
+mmc = CMMCorePlus.instance()
+mmc.loadSystemConfiguration()
 
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Stage Explorer")
+wdg = StageExplorer()
+wdg.poll_stage_position = True
+wdg.show()
 
-        stage = StageWidget("XY")
-        stage.setStep(512)
-        stage.snap_checkbox.setChecked(True)
-        stage_layout = QVBoxLayout()
-        stage_layout.addWidget(stage)
-        stage_layout.addStretch()
+stage = StageWidget("XY")
+stage.setStep(512)
+stage.snap_checkbox.setChecked(True)
+stage.show()
 
-        self.stage_explorer = StageExplorer()
-        self.stage_explorer.poll_stage_position = True
-
-        layout = QHBoxLayout(self)
-        layout.addLayout(stage_layout)
-        layout.addWidget(self.stage_explorer)
-
-        self.stage_explorer.scaleChanged.connect(self._on_scale_changed)
-
-    def _on_scale_changed(self, scale: int) -> None:
-        print(f"Scale changed to: {scale}")
-
-
-if __name__ == "__main__":
-    app = QApplication([])
-    mmc = CMMCorePlus.instance()
-    mmc.loadSystemConfiguration()
-    wdg = Explorer()
-    wdg.show()
-    app.exec()
+app.exec()
