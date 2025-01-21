4aac44ffb0da1ee50a27ad49da0bce366e42a247
Talley Lambert <talley.lambert@gmail.com>
2025-01-21 15:38:33 -0500

    test: add failing test

--- tests/test_config_groups_editor.py
+++ tests/test_config_groups_editor.py
@@ -2,14 +2,22 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from pymmcore_plus.model import ConfigGroup
+
 from pymmcore_widgets import ConfigGroupsEditor
 
 if TYPE_CHECKING:
+    from pymmcore_plus import CMMCorePlus
     from pytestqt.qtbot import QtBot
 
 
-def test_config_groups_editor(qtbot: QtBot) -> None:
-    wdg = ConfigGroupsEditor()
+def test_config_groups_editor(qtbot: QtBot, global_mmcore: CMMCorePlus) -> None:
+    original_groups = ConfigGroup.all_config_groups(global_mmcore)
+    wdg = ConfigGroupsEditor.create_from_core(global_mmcore)
     qtbot.addWidget(wdg)
     wdg.show()
-    assert wdg.isVisible()
+
+    # doing nothing should have returned the same data, unmodified.
+    for new_group in wdg.data():
+        original_group = original_groups[new_group.name]
+        assert new_group == original_group
