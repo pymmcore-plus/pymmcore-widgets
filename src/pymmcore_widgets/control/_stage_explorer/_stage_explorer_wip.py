2303e35423c436c72ec6e6fc31342bc2e430d925
fdrgsp <federico.gasparoli@gmail.com>
2025-02-22 14:07:26 -0500

    remove print

--- src/pymmcore_widgets/control/_stage_explorer/_stage_explorer_wip.py
+++ src/pymmcore_widgets/control/_stage_explorer/_stage_explorer_wip.py
@@ -419,10 +419,6 @@ class StageExplorer(QWidget):
             if cam := self._mmc.getCameraDevice():
                 mirror_x = Keyword.Transpose_MirrorX
                 mirror_y = Keyword.Transpose_MirrorY
-                print(
-                    self._mmc.getProperty(cam, mirror_x),
-                    self._mmc.getProperty(cam, mirror_x) == "1",
-                )
                 S[0, 0] *= -1 if self._mmc.getProperty(cam, mirror_x) == "1" else 1
                 S[1, 1] *= -1 if self._mmc.getProperty(cam, mirror_y) == "1" else 1
             RS = R @ S
