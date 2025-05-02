6d6fab309c44d9e1a3b1771d71c241232c51f3b7
Talley Lambert <talley.lambert@gmail.com>
2025-05-02 14:00:59 -0400

    updates

--- examples/stage_viewer_widget.py
+++ examples/stage_viewer_widget.py
@@ -1,35 +1,27 @@
 # NOTE: run in ipython with `%run examples/stage_viewer_widget.py`
 
 import numpy as np
-from vispy import scene
+from qtpy.QtGui import QMouseEvent
 
 from pymmcore_widgets.control._stage_explorer._stage_viewer import StageViewer
 
-img = np.random.randint(0, 255, (256, 256))
+img = np.random.randint(0, 255, (256, 256), dtype=np.uint8)
+
+stage_viewer = StageViewer()
+stage_viewer.show()
+stage_viewer.add_image(img)
+T = np.eye(4)
+T[3, :3] = 400, 250, 0  # x, y, z shift
+stage_viewer.add_image(img, T.T)
+stage_viewer.zoom_to_fit()
 
 
-class SV(StageViewer):
-    """Stage viewer widget with mouse press event."""
-
-    def __init__(self):
-        super().__init__()
-
-        self.add_image(img)
-        self.reset_view()
-
-        self.canvas.events.mouse_press.connect(self._on_mouse_press)
-
-    def _on_mouse_press(self, event) -> None:
-        """Handle the mouse press event."""
-        canvas_pos = (event.pos[0], event.pos[1])
-        world_pos = self._tform().map(canvas_pos)[:2]
-        print()
-        print(world_pos)
-
-    def _tform(self) -> scene.transforms.BaseTransform:
-        """Return the transform from canvas to scene."""
-        return list(self._get_images())[-1].transforms.get_transform("canvas", "scene")
-
-
-wdg = SV()
-wdg.show()
+@stage_viewer.canvas.events.mouse_press.connect
+def _on_mouse_press(event: QMouseEvent) -> None:
+    """Handle the mouse press event."""
+    canvas_pos = (event.pos[0], event.pos[1])
+    last_image = list(stage_viewer._get_images())[-1]
+    tform = last_image.transforms.get_transform("canvas", "scene")
+    world_pos = tform.map(canvas_pos)[:2]
+    print()
+    print(world_pos)
