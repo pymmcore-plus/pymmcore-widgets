d26ffde433b952e9741465e149c082f65e195925
Talley Lambert <talley.lambert@gmail.com>
2025-06-30 19:08:12 -0400

    feat: add ConfigGroupsTree (#448)
    * feat: add ConfigGroupsTree
    
    * starting tests

--- src/pymmcore_widgets/config_presets/__init__.py
+++ src/pymmcore_widgets/config_presets/__init__.py
@@ -4,8 +4,10 @@ from ._group_preset_widget._group_preset_table_widget import GroupPresetTableWid
 from ._objectives_pixel_configuration_widget import ObjectivesPixelConfigurationWidget
 from ._pixel_configuration_widget import PixelConfigurationWidget
 from ._qmodel._config_model import QConfigGroupsModel
+from ._views._config_groups_tree import ConfigGroupsTree
 
 __all__ = [
+    "ConfigGroupsTree",
     "GroupPresetTableWidget",
     "ObjectivesPixelConfigurationWidget",
     "PixelConfigurationWidget",
