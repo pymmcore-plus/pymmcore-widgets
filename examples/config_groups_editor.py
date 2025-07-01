b951b88b4f77b7eb02150c4b7da14ecc116cbe4a
Talley Lambert <talley.lambert@gmail.com>
2025-06-30 21:41:20 -0400

    remove file

--- examples/config_groups_editor.py
+++ examples/config_groups_editor.py
@@ -1,11 +1,8 @@
 from pymmcore_plus import CMMCorePlus
 from qtpy.QtCore import QModelIndex
-from qtpy.QtWidgets import QApplication, QSplitter, QTreeView
+from qtpy.QtWidgets import QApplication, QSplitter
 
-from pymmcore_widgets import ConfigGroupsEditor
-from pymmcore_widgets.config_presets._qmodel._property_setting_delegate import (
-    PropertySettingDelegate,
-)
+from pymmcore_widgets import ConfigGroupsEditor, ConfigGroupsTree
 
 app = QApplication([])
 core = CMMCorePlus()
@@ -15,12 +12,9 @@ cfg = ConfigGroupsEditor.create_from_core(core)
 cfg.setCurrentPreset("Channel", "FITC")
 
 # right-hand tree view showing the *same* model
-tree = QTreeView()
+tree = ConfigGroupsTree()
 tree.setModel(cfg._model)
 tree.expandRecursively(QModelIndex())
-tree.setColumnWidth(0, 180)
-# make values in in the tree editable
-tree.setItemDelegateForColumn(2, PropertySettingDelegate(tree))
 
 
 splitter = QSplitter()
