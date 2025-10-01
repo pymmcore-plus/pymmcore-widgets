f1e216927dadcd4c7bd4b3b90c1966fdaac5c9f6
fdrgsp <federico.gasparoli@gmail.com>
2025-10-01 10:46:52 -0400

    test: update test

--- tests/test_image_preview.py
+++ tests/test_image_preview.py
@@ -41,29 +41,56 @@ SEQ = [
     # sequencable
     useq.MDASequence(
         time_plan=useq.TIntervalLoops(interval=0, loops=3),
-        channels=(["DAPI"]),
+        channels=(["FITC"]),
     ),
     # non-sequencable
     useq.MDASequence(
         time_plan=useq.TIntervalLoops(interval=0.2, loops=3),
-        channels=(["DAPI"]),
+        channels=(["FITC"]),
     ),
 ]
 
 
 @pytest.mark.parametrize("seq", SEQ)
-# @pytest.mark.parametrize("use_with_mda", [True, False])
+@pytest.mark.parametrize("use_with_mda", [True, False])
 def test_image_preview_with_mda_sequences(
-    qtbot: "QtBot", seq: useq.MDASequence, use_with_mda: bool = True
+    qtbot: "QtBot", seq: useq.MDASequence, use_with_mda: bool
 ):
-    """Test that the exposure widget works."""
+    """Test ImagePreview widget MDA frame updates based on use_with_mda setting."""
     mmcore = CMMCorePlus.instance()
     widget = ImagePreview(use_with_mda=use_with_mda)
     qtbot.addWidget(widget)
     assert widget._mmc is mmcore
+    assert mmcore.mda.engine is not None
+    assert mmcore.mda.engine.use_hardware_sequencing
 
     assert mmcore.mda.engine.use_hardware_sequencing
 
-    with wait_signal(qtbot, mmcore.mda.events.sequenceStarted):
+    # Get initial image state
+    with wait_signal(qtbot, mmcore.events.imageSnapped):
+        mmcore.snap()
+    initial_image = widget._canvas.render()
+    assert widget.image is not None
+
+    # Run MDA sequence and wait for it to complete
+    with wait_signal(qtbot, mmcore.mda.events.sequenceFinished):
         mmcore.mda.run(seq)
+
+    # Get final image state
+    final_image = widget._canvas.render()
+
+    # Test behavior based on use_with_mda setting
+    if use_with_mda:
+        # When use_with_mda=True, _on_frame_ready should update the image
+        # The image should be different from initial (updated during MDA)
+        assert not np.allclose(initial_image, final_image), (
+            "Image should be updated during MDA when use_with_mda=True"
+        )
+    else:
+        # When use_with_mda=False, _on_frame_ready should NOT update the image
+        # The image should remain the same as initial snap
+        assert np.allclose(initial_image, final_image), (
+            "Image should NOT be updated during MDA when use_with_mda=False"
+        )
+
     assert widget.use_with_mda is use_with_mda
