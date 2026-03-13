8c61b45b222e48bbee7d2e956ea4bc1caaaf48c4
Talley Lambert <talley.lambert@gmail.com>
2026-03-13 14:46:10 -0400

    feat: enable dynamic configuration updates based on command line argument

--- examples/config_groups_editor.py
+++ examples/config_groups_editor.py
@@ -1,3 +1,5 @@
+import sys
+
 from pymmcore_plus import CMMCorePlus
 from qtpy.QtWidgets import QApplication
 
@@ -8,7 +10,8 @@ core = CMMCorePlus()
 core.loadSystemConfiguration()
 
 cfg = ConfigGroupsEditor()
-cfg.update_from_core(core, update_configs=False)
+with_cfg = sys.argv[1] in ("1", "true") if len(sys.argv) > 1 else False
+cfg.update_from_core(core, update_configs=with_cfg)
 cfg.resize(900, 550)
 cfg.show()
 
